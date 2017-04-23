b150fee179d84a804fe8068f74ad64cdc3c6b19f
MarcoFalke <falke.marco@gmail.com>
2017-04-23 17:43:00 +0200

    Merge #10023: [tests] remove maxblocksinflight.py (functionality covered by other test)
    5f4bcf2 [tests] Remove maxblocksinflight testcase (John Newbery)
    
    Tree-SHA512: 827c8b12f4b52684a973bbfc508c5906e640572e22a96b9420a7aea86ad8d4aa4d6fcc2bb091f747e2fdd18c897e0456baff254bd5e3ceaf721bd3f09a2fd60b
    
    delete maxblocksinflight.py
    
    Signed-off-by: Pasta <pasta@dashboost.org>

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -136,7 +136,6 @@ EXTENDED_SCRIPTS = [
     'txindex.py',
     'forknotify.py',
     'invalidateblock.py',
-    'maxblocksinflight.py',
     'p2p-acceptblock.py', # NOTE: needs axe_hash to pass
 ]
 
