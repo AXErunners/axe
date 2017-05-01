65553019fb39959fed3095e0fa19f9c6af7af69c
Wladimir J. van der Laan <laanwj@gmail.com>
2017-05-01 09:16:17 +0200

    Merge #10290: Add -stopatheight for benchmarking
    b297426 Add -stopatheight for benchmarking (Pieter Wuille)
    
    Tree-SHA512: e8a4cca7fc1accd0dcd3a0eda97839fc34c61f25d6302082a8d6ecf9a0291b4abacbde16a0ecba7bdd2a56dd0c8d4d54300ad3b3478386da21dd7697acce381c

--- src/validation.cpp
+++ src/validation.cpp
@@ -2936,6 +2936,9 @@ bool ActivateBestChain(CValidationState &state, const CChainParams& chainparams,
         return false;
     }
 
+    int nStopAtHeight = GetArg("-stopatheight", DEFAULT_STOPATHEIGHT);
+    if (nStopAtHeight && pindexNewTip->nHeight >= nStopAtHeight) StartShutdown();
+
     return true;
 }
 
