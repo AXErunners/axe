177ba7010491a47ed25a149077fd9465f09a53aa
-k <slowdive@me.com>
2018-01-30 02:56:35 -0500

    edit

--- src/qt/rpcconsole.cpp
+++ src/qt/rpcconsole.cpp
@@ -431,12 +431,12 @@ void RPCConsole::on_tabWidget_currentChanged(int index)
 void RPCConsole::on_openDebugLogfileButton_clicked()
 {
     GUIUtil::openDebugLogfile();
-
+}
 void RPCConsole::on_openConfigurationfileButton_clicked()
   {
      GUIUtil::openConfigfile();
   }
-}
+
 
 void RPCConsole::scrollToEnd()
 {
