5f9f07cc913597203de5222050ce0733461d7cf6
-k <slowdive@me.com>
2018-01-30 22:13:57 -0500

    restyle std=c++11

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -426,7 +426,7 @@ int ReadHTTPHeader(std::basic_istream<char>& stream, map<string, string>& mapHea
         {
             string strHeader = str.substr(0, nColon);
             boost::trim(strHeader);
-            boost::to_lower(strHeader);
+            transform(strHeader.begin(), strHeader.end(), strHeader.begin(), ::tolower);
             string strValue = str.substr(nColon+1);
             boost::trim(strValue);
             mapHeadersRet[strHeader] = strValue;
