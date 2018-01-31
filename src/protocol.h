664b8da0da9509c36fcf097dd836847915f114d9
-k <slowdive@me.com>
2018-01-31 17:32:35 -0500

    revert

--- src/protocol.h
+++ src/protocol.h
@@ -3,6 +3,10 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#ifndef __cplusplus
+# error This header can only be compiled as C++.
+#endif
+
 #ifndef __INCLUDED_PROTOCOL_H__
 #define __INCLUDED_PROTOCOL_H__
 
