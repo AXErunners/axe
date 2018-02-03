21c5bc1be36aa51d85c55b38e9d3e6998a7c1e2a
-k <slowdive@me.com>
2018-02-02 19:30:45 -0500

    config.h

--- share/qt/src/clientversion.h
+++ share/qt/src/clientversion.h
@@ -12,7 +12,7 @@
 // These need to be macros, as version.cpp's and bitcoin-qt.rc's voodoo requires it
 #define CLIENT_VERSION_MAJOR       1
 #define CLIENT_VERSION_MINOR       0
-#define CLIENT_VERSION_REVISION    3
+#define CLIENT_VERSION_REVISION    4
 #define CLIENT_VERSION_BUILD       0
 
 #endif //HAVE_CONFIG_H
