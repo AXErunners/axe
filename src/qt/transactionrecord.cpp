3d2e0ac7fe03d4a0d4dd10369c4b18a836cb6a06
Wladimir J. van der Laan <laanwj@gmail.com>
2018-03-07 16:48:09 +0100

    Merge #12620: Remove TransactionTableModel::TxIDRole
    3b26b6af7 qt: Remove TransactionTableModel::TxIDRole (João Barbosa)
    
    Pull request description:
    
      The role `TxIDRole` is a duplicate of `TxHashRole`. This change favours `TxHashRole`.
    
    Tree-SHA512: ad35933eae1cb6b242b25b8940d662c2c79c766732d76fdd410c80230ec084969294a8e5a126794707992a566076ef4452b592050f7af6c4fa7742891090803d

--- src/qt/transactionrecord.cpp
+++ src/qt/transactionrecord.cpp
@@ -375,7 +375,7 @@ bool TransactionRecord::statusUpdateNeeded(int chainLockHeight) const
         || (!status.lockedByChainLocks && status.cachedChainLockHeight != chainLockHeight);
 }
 
-QString TransactionRecord::getTxID() const
+QString TransactionRecord::getTxHash() const
 {
     return QString::fromStdString(hash.ToString());
 }
