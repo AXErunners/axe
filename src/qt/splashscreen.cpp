62609867e80b85e252f7365da164a031608ecf18
-k <slowdive@me.com>
2018-05-26 04:52:51 -0400

    change text color

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -139,7 +139,7 @@ static void InitMessage(SplashScreen *splash, const std::string &message)
         Qt::QueuedConnection,
         Q_ARG(QString, QString::fromStdString(message)),
         Q_ARG(int, Qt::AlignBottom|Qt::AlignHCenter),
-        Q_ARG(QColor, QColor(55,55,55)));
+        Q_ARG(QColor, QColor(255,26,26)));
 }
 
 static void ShowProgress(SplashScreen *splash, const std::string &title, int nProgress)
