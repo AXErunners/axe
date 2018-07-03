09bd9a6fd41f8ed157bb5266099e6eabc78db6e9
-k <slowdive@me.com>
2018-07-03 07:03:04 -0400

    add err output

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# Copyright (c) 2013-2016 The Bitcoin Core developers
+# Copyright (c) 2017-2018 The AXE Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
 cat << "EOF"
      ___           ___           ___                    ___           ___           ___           ___
     /\  \         |\__\         /\  \                  /\  \         /\  \         /\  \         /\  \
@@ -20,4 +24,6 @@ if [ -z ${LIBTOOLIZE} ] && GLIBTOOLIZE="`which glibtoolize 2>/dev/null`"; then
   LIBTOOLIZE="${GLIBTOOLIZE}"
   export LIBTOOLIZE
 fi
+which autoreconf >/dev/null || \
+  (echo "configuration failed, please install autoconf first" && exit 1)
 autoreconf --install --force --warnings=all
