62bc298df31f988b3306df2a9cd1741aa0c04946
-k <slowdive@me.com>
2018-09-03 14:01:00 -0400

    update c header

--- src/governance-exceptions.h
+++ src/governance-exceptions.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2014-2017 The AXE Core developers
+// Copyright (c) 2014-2017 The Dash Core developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -76,7 +76,7 @@ public:
 
     virtual ~CGovernanceException() throw() {}
 
-    virtual const char* what() const throw() override 
+    virtual const char* what() const throw() override
     {
         return strMessage.c_str();
     }
