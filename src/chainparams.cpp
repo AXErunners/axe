6d12ffde8f4fb2987d253f7a5879f130ac062fcc
-k <slowdive@me.com>
2018-11-17 19:32:15 -0500

    add checkpoint
    block 161083

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -257,11 +257,12 @@ public:
             ( 86382, uint256S("0x00000000003badda6d1a8eeed581ccb67d6b556be55c5ec85e9a0329dfc56f74"))
             ( 93105, uint256S("0x0000000000069b605e286b9c195bd400df907bd2fcf87398c2a0dfff5df0fbf3"))
             ( 100315, uint256S("0x0000000000002f905a48b6b6e26d77ea86483d07f02081d2ab1de5f6279a0311"))
+            ( 161083, uint256S("0x0000000000092122485e390355bf4345ee51b0b14ba0d8c8ee6571ab41cca881"))
         };
 
         chainTxData = ChainTxData{
-            1532912841, // * UNIX timestamp of last known number of transactions
-            77570,    // * total number of transactions between genesis and that timestamp
+            1542500827, // * UNIX timestamp of last known number of transactions
+            106525,    // * total number of transactions between genesis and that timestamp
                         //   (the tx=... number in the SetBestChain debug.log lines)
             0.1         // * estimated number of transactions per second after that timestamp
         };
