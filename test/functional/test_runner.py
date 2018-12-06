5a0764e17331a90447ad1aba8b3b7c93b2e779d6
Wladimir J. van der Laan <laanwj@gmail.com>
2018-12-06 17:42:52 +0100

    Merge #14670: http: Fix HTTP server shutdown
    28479f926f21f2a91bec5a06671c60e5b0c55532 qa: Test bitcond shutdown (João Barbosa)
    8d3f46ec3938e2ba17654fecacd1d2629f9915fd http: Remove timeout to exit event loop (João Barbosa)
    e98a9eede2fb48ff33a020acc888cbcd83e24bbf http: Remove unnecessary event_base_loopexit call (João Barbosa)
    6b13580f4e3842c11abd9b8bee7255fb2472b6fe http: Unlisten sockets after all workers quit (João Barbosa)
    18e968581697078c36a3c3818f8906cf134ccadd http: Send "Connection: close" header if shutdown is requested (João Barbosa)
    02e1e4eff6cda0bfc24b455a7c1583394cbff6eb rpc: Add wait argument to stop (João Barbosa)
    
    Pull request description:
    
      Fixes #11777. Reverts #11006. Replaces #13501.
    
      With this change the HTTP server will exit gracefully, meaning that all requests will finish processing and sending the response, even if this means to wait more than 2 seconds (current time allowed to exit the event loop).
    
      Another small change is that connections are accepted even when the server is stopping, but HTTP requests are rejected. This can be improved later, especially if chunked replies are implemented.
    
      Briefly, before this PR, this is the order or events when a request arrives (RPC `stop`):
       1. `bufferevent_disable(..., EV_READ)`
       2. `StartShutdown()`
       3. `evhttp_del_accept_socket(...)`
       4. `ThreadHTTP` terminates (event loop exits) because there are no active or pending events thanks to 1. and 3.
       5. client doesn't get the response thanks to 4.
    
      This can be verified by applying
      ```diff
           // Event loop will exit after current HTTP requests have been handled, so
           // this reply will get back to the client.
           StartShutdown();
      +    MilliSleep(2000);
           return "Bitcoin server stopping";
       }
      ```
      and checking the log output:
      ```
          Received a POST request for / from 127.0.0.1:62443
          ThreadRPCServer method=stop user=__cookie__
          Interrupting HTTP server
      **  Exited http event loop
          Interrupting HTTP RPC server
          Interrupting RPC
          tor: Thread interrupt
          Shutdown: In progress...
          torcontrol thread exit
          Stopping HTTP RPC server
          addcon thread exit
          opencon thread exit
          Unregistering HTTP handler for / (exactmatch 1)
          Unregistering HTTP handler for /wallet/ (exactmatch 0)
          Stopping RPC
          RPC stopped.
          Stopping HTTP server
          Waiting for HTTP worker threads to exit
          msghand thread exit
          net thread exit
    
          ... sleep 2 seconds ...
    
          Waiting for HTTP event thread to exit
          Stopped HTTP server
      ```
    
      For this reason point 3. is moved right after all HTTP workers quit. In that moment HTTP replies are queued in the event loop which keeps spinning util all connections are closed. In order to trigger the server side close with keep alive connections (implicit in HTTP/1.1) the header `Connection: close` is sent if shutdown was requested. This can be tested by
      ```
      bitcoind -regtest
      nc localhost 18443
      POST / HTTP/1.1
      Authorization: Basic ...
      Content-Type: application/json
      Content-Length: 44
    
      {"jsonrpc": "2.0","method":"stop","id":123}
      ```
    
      Summing up, this PR:
       - removes explicit event loop exit — event loop exits once there are no active or pending events
       - changes the moment the listening sockets are removed — explained above
       - sends header `Connection: close` on active requests when shutdown was requested which is relevant when it's a persistent connection (default in HTTP 1.1) — libevent is aware of this header and closes the connection gracefully
       - removes event loop explicit break after 2 seconds timeout
    
    Tree-SHA512: 4dac1e86abe388697c1e2dedbf31fb36a394cfafe5e64eadbf6ed01d829542785a8c3b91d1ab680d3f03f912d14fc87176428041141441d25dcb6c98a1e069d8

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -137,6 +137,7 @@ BASE_SCRIPTS= [
     'resendwallettransactions.py',
     'minchainwork.py',
     'p2p-acceptblock.py', # NOTE: needs axe_hash to pass
+    'feature_shutdown.py',
 ]
 
 EXTENDED_SCRIPTS = [
