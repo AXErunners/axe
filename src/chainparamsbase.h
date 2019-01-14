1307818f18f72ed19faf53e9fab5004c21548060
-k <slowdive@me.com>
2019-01-14 15:36:49 -0800

    update name

--- src/chainparamsbase.h
+++ src/chainparamsbase.h
@@ -10,7 +10,7 @@
 
 /**
  * CBaseChainParams defines the base parameters (shared between axe-cli and axed)
- * of a given instance of the AXE system.
+ * of a given instance of the Axe system.
  */
 class CBaseChainParams
 {
