1e977604c66f27632023259f5be4a1feebddb190
-k <slowdive@me.com>
2019-01-20 15:41:00 -0800

    update name for ci scripts

--- ci/matrix.sh
+++ ci/matrix.sh
@@ -57,7 +57,7 @@ elif [ "$BUILD_TARGET" = "linux32" ]; then
   export PACKAGES="g++-multilib bc python3-zmq"
   export DEP_OPTS="NO_QT=1"
   export BITCOIN_CONFIG="--enable-zmq --enable-glibc-back-compat --enable-reduce-exports LDFLAGS=-static-libstdc++"
-  export USE_SHELL="/bin/dash"
+  export USE_SHELL="/bin/axe"
   export PYZMQ=true
   export RUN_TESTS=true
 elif [ "$BUILD_TARGET" = "linux64" ]; then
@@ -65,7 +65,7 @@ elif [ "$BUILD_TARGET" = "linux64" ]; then
   export PACKAGES="bc python3-zmq"
   export DEP_OPTS="NO_QT=1 NO_UPNP=1 DEBUG=1"
   export BITCOIN_CONFIG="--enable-zmq --enable-glibc-back-compat --enable-reduce-exports"
-  export CPPFLAGS="-DDEBUG_LOCKORDER -DENABLE_DASH_DEBUG"
+  export CPPFLAGS="-DDEBUG_LOCKORDER -DENABLE_AXE_DEBUG"
   export PYZMQ=true
   export RUN_TESTS=true
 elif [ "$BUILD_TARGET" = "linux64_nowallet" ]; then
