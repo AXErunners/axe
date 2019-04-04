209656f06c1de4c79f2176980827e4104ae28830
-k <slowdive@me.com>
2019-04-04 16:46:20 -0700

    drop hardcoded spork15

--- src/spork.cpp
+++ src/spork.cpp
@@ -28,7 +28,7 @@ std::map<int, int64_t> mapSporkDefaults = {
     {SPORK_12_RECONSIDER_BLOCKS,             0},             // 0 BLOCKS
     {SPORK_14_REQUIRE_SENTINEL_FLAG,         4070908800ULL}, // OFF
     {SPORK_16_INSTANTSEND_AUTOLOCKS,         0},             // ON
-    {SPORK_15_DETERMINISTIC_MNS_ENABLED,     213696},        // Hardened to mainnet activation height
+    {SPORK_15_DETERMINISTIC_MNS_ENABLED,     0},             // ON
     {SPORK_17_QUORUM_DKG_ENABLED,            4070908800ULL}, // OFF
 };
 
