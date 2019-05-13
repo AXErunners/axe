5c8e0cc93c00fcafe28a4ed3af18221dbde7aa65
-k <slowdive@me.com>
2019-05-13 00:11:44 -0700

    Update copyright headers

--- src/bench/crypto_hash.cpp
+++ src/bench/crypto_hash.cpp
@@ -1,5 +1,5 @@
 // Copyright (c) 2016 The Bitcoin Core developers
-// Copyright (c) 2018 The Axe Core developers
+// Copyright (c) 2018-2019 The Axe Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
