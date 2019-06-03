dd99d1289e7a044a9b3a221d6ed3b48ad9f10556
-k <slowdive@me.com>
2019-06-03 14:26:19 -0700

    Add basic Qt wallet test

--- ci/test_unittests.sh
+++ ci/test_unittests.sh
@@ -18,10 +18,16 @@ export LD_LIBRARY_PATH=$BUILD_DIR/depends/$HOST/lib
 export WINEDEBUG=fixme-all
 export BOOST_TEST_LOG_LEVEL=test_suite
 
-cd build-ci/axecore-$BUILD_TARGET
+cd build-ci/dashcore-$BUILD_TARGET
+
+if [ "$RUN_TESTS" = "true" -a "${DEP_OPTS#*NO_QT=1}" = "$DEP_OPTS" ]; then
+  export DISPLAY=:99.0;
+  /sbin/start-stop-daemon --start --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac;
+fi
+
 if [ "$DIRECT_WINE_EXEC_TESTS" = "true" ]; then
   # Inside Docker, binfmt isn't working so we can't trust in make invoking windows binaries correctly
-  wine ./src/test/test_axe.exe
+  wine ./src/test/test_dash.exe
 else
   make $MAKEJOBS check VERBOSE=1
 fi
