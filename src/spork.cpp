752db7c10a2c4e5096791b27d212d927a6bda0fa
-k <slowdive@me.com>
2019-11-08 11:49:13 -0800

    Update spork.cpp

--- src/spork.cpp
+++ src/spork.cpp
@@ -21,13 +21,13 @@ std::vector<CSporkDef> sporkDefs = {
     MAKE_SPORK_DEF(SPORK_3_INSTANTSEND_BLOCK_FILTERING,    0),             // ON
     MAKE_SPORK_DEF(SPORK_5_INSTANTSEND_MAX_VALUE,          1000),          // 1000 Dash
     MAKE_SPORK_DEF(SPORK_6_NEW_SIGS,                       4070908800ULL), // OFF
-    MAKE_SPORK_DEF(SPORK_9_SUPERBLOCKS_ENABLED,            0},             // ON
+    MAKE_SPORK_DEF(SPORK_9_SUPERBLOCKS_ENABLED,            0),             // ON
     MAKE_SPORK_DEF(SPORK_12_RECONSIDER_BLOCKS,             0),             // 0 BLOCKS
-    MAKE_SPORK_DEF(SPORK_15_DETERMINISTIC_MNS_ENABLED,     0},             // ON
-    MAKE_SPORK_DEF(SPORK_16_INSTANTSEND_AUTOLOCKS,         0},             // ON
-    MAKE_SPORK_DEF(SPORK_17_QUORUM_DKG_ENABLED,            0},             // ON
-    MAKE_SPORK_DEF(SPORK_19_CHAINLOCKS_ENABLED,            368964},        // Hardcoded
-    MAKE_SPORK_DEF(SPORK_20_INSTANTSEND_LLMQ_BASED,        368965},        // Hardcoded
+    MAKE_SPORK_DEF(SPORK_15_DETERMINISTIC_MNS_ENABLED,     0),             // ON
+    MAKE_SPORK_DEF(SPORK_16_INSTANTSEND_AUTOLOCKS,         0),             // ON
+    MAKE_SPORK_DEF(SPORK_17_QUORUM_DKG_ENABLED,            0),             // ON
+    MAKE_SPORK_DEF(SPORK_19_CHAINLOCKS_ENABLED,            368964),        // Hardcoded
+    MAKE_SPORK_DEF(SPORK_20_INSTANTSEND_LLMQ_BASED,        368965),        // Hardcoded
 };
 
 CSporkManager sporkManager;
