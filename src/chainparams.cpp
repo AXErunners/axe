bbaeb8b82c3be8834576a67d6537871d1bf3ce75
-k <slowdive@me.com>
2019-11-20 18:45:16 -0800

    Fix syntax/CCheckpointData

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -364,6 +364,7 @@ public:
 
         checkpointData = (CCheckpointData) {
             boost::assign::map_list_of
+            {
                 {     0, uint256S("0x00000c33631ca6f2f61368991ce2dc03306b5bb50bf7cede5cfbba6db38e52e6")},
                 {  1000, uint256S("0x000001c94f32e43dc459bc522e343b41f179b7b486e155e819c3f251ee4a6d8d")},
                 {  5000, uint256S("0x0000002e22464e17a0140f686bfdd7794ade570f71d884098302cba16b6059d5")},
