242ef07752e642c944a2c5df3e31263be5991678
-k <slowdive@me.com>
2019-11-23 12:38:17 -0800

    Change dash -> axe

--- test/functional/multi_rpc.py
+++ test/functional/multi_rpc.py
@@ -28,7 +28,7 @@ class HTTPBasicsTest (BitcoinTestFramework):
         with open(os.path.join(self.options.tmpdir+"/node0", "axe.conf"), 'a', encoding='utf8') as f:
             f.write(rpcauth+"\n")
             f.write(rpcauth2+"\n")
-        with open(os.path.join(self.options.tmpdir+"/node1", "dash.conf"), 'a', encoding='utf8') as f:
+        with open(os.path.join(self.options.tmpdir+"/node1", "axe.conf"), 'a', encoding='utf8') as f:
             f.write(rpcuser+"\n")
             f.write(rpcpassword+"\n")
 
