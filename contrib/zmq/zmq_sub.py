dashpay/dash

Few fixes in docs/comments

contrib/zmq/zmq_sub.py
@@ -7,7 +7,7 @@
     ZMQ example using python3's asyncio
 
     Dash should be started with the command line arguments:
-        dashd-testnet -daemon \
+        dashd -testnet -daemon \
                 -zmqpubrawtx=tcp://127.0.0.1:28332 \
                 -zmqpubrawblock=tcp://127.0.0.1:28332 \
                 -zmqpubhashtx=tcp://127.0.0.1:28332 \

AXErunners/axe

contrib/zmq/zmq_sub.py
@@ -7,7 +7,7 @@
     ZMQ example using python3's asyncio
 
     Axe should be started with the command line arguments:
-        axed-testnet -daemon \
+        axed -testnet -daemon \
                 -zmqpubrawtx=tcp://127.0.0.1:28332 \
                 -zmqpubrawblock=tcp://127.0.0.1:28332 \
                 -zmqpubhashtx=tcp://127.0.0.1:28332 \