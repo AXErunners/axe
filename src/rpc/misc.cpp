dashpay/dash

Update/unify `debug` and `logging` rpc descriptions

src/rpc/misc.cpp
@@ -134,14 +134,15 @@ UniValue debug(const JSONRPCRequest& request)
         throw std::runtime_error(
             "debug \"category\"\n"
             "Change debug category on the fly. Specify single category or use '+' to specify many.\n"
+            "The valid debug categories are: " + ListLogCategories() + ".\n"
+            "libevent logging is configured on startup and cannot be modified by this RPC during runtime.\n"
+            "There are also few meta-categories:\n"
+            " - \"all\" (or \"1\") activates all categories at once;\n"
+            " - \"dash\" activates all Dash-specific categories at once;\n"
+            " - \"none\" (or \"0\") deactivates all categories at once.\n"
+            "Note: If specified category doesn't match any of the above, no error is thrown.\n"
             "\nArguments:\n"
-            "1. \"category\"          (string, required) The name of the debug category to turn on. Can be one of the following:\n"
-            "                       addrman, bench, cmpctblock, coindb, db, http, leveldb, libevent, lock, mempool,\n"
-            "                       mempoolrej, net, proxy, prune, qt, rand, reindex, rpc, selectcoins, tor, zmq, dash\n"
-            "                       (or specifically: chainlocks, gobject, instantsend, keepass, llmq, llmq-dkg, llmq-sigs,\n"
-            "                       mnpayments, mnsync, privatesend, spork).\n"
-            "                       Can also use \"1\" to turn all categories on at once and \"0\" to turn them off.\n"
-            "                       Note: If specified category doesn't match any of the above, no error is thrown.\n"
+            "1. \"category\"          (string, required) The name of the debug category to turn on.\n"
             "\nResult:\n"
             "  result               (string) \"Debug mode: \" followed by the specified category.\n"
             "\nExamples:\n"
@@ -1223,9 +1224,13 @@ UniValue logging(const JSONRPCRequest& request)
             "Gets and sets the logging configuration.\n"
             "When called without an argument, returns the list of categories that are currently being debug logged.\n"
             "When called with arguments, adds or removes categories from debug logging.\n"
-            "The valid logging categories are: " + ListLogCategories() + "\n"
-            "libevent logging is configured on startup and cannot be modified by this RPC during runtime."
-            "Arguments:\n"
+            "The valid logging categories are: " + ListLogCategories() + ".\n"
+            "libevent logging is configured on startup and cannot be modified by this RPC during runtime.\n"
+            "There are also few meta-categories:\n"
+            " - \"all\" (or \"1\") activates all categories at once;\n"
+            " - \"dash\" activates all Dash-specific categories at once.\n"
+            "To deactivate all categories at once you can specify \"all\" in <exclude>.\n"
+            "\nArguments:\n"
             "1. \"include\" (array of strings) add debug logging for these categories.\n"
             "2. \"exclude\" (array of strings) remove debug logging for these categories.\n"
             "\nResult: <categories>  (string): a list of the logging categories that are active.\n"

AXErunners/axe

src/rpc/misc.cpp
@@ -134,14 +134,15 @@ UniValue debug(const JSONRPCRequest& request)
         throw std::runtime_error(
             "debug \"category\"\n"
             "Change debug category on the fly. Specify single category or use '+' to specify many.\n"
+            "The valid debug categories are: " + ListLogCategories() + ".\n"
+            "libevent logging is configured on startup and cannot be modified by this RPC during runtime.\n"
+            "There are also few meta-categories:\n"
+            " - \"all\" (or \"1\") activates all categories at once;\n"
+            " - \"axe\" activates all Axe-specific categories at once;\n"
+            " - \"none\" (or \"0\") deactivates all categories at once.\n"
+            "Note: If specified category doesn't match any of the above, no error is thrown.\n"
             "\nArguments:\n"
-            "1. \"category\"          (string, required) The name of the debug category to turn on. Can be one of the following:\n"
-            "                       addrman, bench, cmpctblock, coindb, db, http, leveldb, libevent, lock, mempool,\n"
-            "                       mempoolrej, net, proxy, prune, qt, rand, reindex, rpc, selectcoins, tor, zmq, axe\n"
-            "                       (or specifically: chainlocks, gobject, instantsend, keepass, llmq, llmq-dkg, llmq-sigs,\n"
-            "                       mnpayments, mnsync, privatesend, spork).\n"
-            "                       Can also use \"1\" to turn all categories on at once and \"0\" to turn them off.\n"
-            "                       Note: If specified category doesn't match any of the above, no error is thrown.\n"
+            "1. \"category\"          (string, required) The name of the debug category to turn on.\n"
             "\nResult:\n"
             "  result               (string) \"Debug mode: \" followed by the specified category.\n"
             "\nExamples:\n"
@@ -1223,9 +1224,13 @@ UniValue logging(const JSONRPCRequest& request)
             "Gets and sets the logging configuration.\n"
             "When called without an argument, returns the list of categories that are currently being debug logged.\n"
             "When called with arguments, adds or removes categories from debug logging.\n"
-            "The valid logging categories are: " + ListLogCategories() + "\n"
-            "libevent logging is configured on startup and cannot be modified by this RPC during runtime."
-            "Arguments:\n"
+            "The valid logging categories are: " + ListLogCategories() + ".\n"
+            "libevent logging is configured on startup and cannot be modified by this RPC during runtime.\n"
+            "There are also few meta-categories:\n"
+            " - \"all\" (or \"1\") activates all categories at once;\n"
+            " - \"axe\" activates all Axe-specific categories at once.\n"
+            "To deactivate all categories at once you can specify \"all\" in <exclude>.\n"
+            "\nArguments:\n"
             "1. \"include\" (array of strings) add debug logging for these categories.\n"
             "2. \"exclude\" (array of strings) remove debug logging for these categories.\n"
             "\nResult: <categories>  (string): a list of the logging categories that are active.\n"