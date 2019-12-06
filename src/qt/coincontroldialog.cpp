c35209ac402ea498041cf26d41d6dde25c1dc056
-k <slowdive@me.com>
2019-12-05 22:00:36 -0800

    s/duff/hak

--- src/qt/coincontroldialog.cpp
+++ src/qt/coincontroldialog.cpp
@@ -628,7 +628,7 @@ void CoinControlDialog::updateLabels(WalletModel *model, QDialog* dialog)
     // how many satoshis the estimated fee can vary per byte we guess wrong
     double dFeeVary = (double)nPayFee / nBytes;
 
-    QString toolTip4 = tr("Can vary +/- %1 duff(s) per input.").arg(dFeeVary);
+    QString toolTip4 = tr("Can vary +/- %1 hak(s) per input.").arg(dFeeVary);
 
     l3->setToolTip(toolTip4);
     l4->setToolTip(toolTip4);
