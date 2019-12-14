bytzck/BYTZ

Dashify BITCOINCLI

test/functional/test_runner.py
@@ -295,7 +295,7 @@ def run_tests(test_list, src_dir, build_dir, exeext, tmpdir, jobs=1, enable_cove
     #Set env vars
     if "BITCOIND" not in os.environ:
         os.environ["BITCOIND"] = build_dir + '/src/dashd' + exeext
-        os.environ["BITCOINCLI"] = build_dir + '/src/bitcoin-cli' + exeext
+        os.environ["BITCOINCLI"] = build_dir + '/src/dash-cli' + exeext
 
     tests_dir = src_dir + '/test/functional/'
 

AXErunners/axe

test/functional/test_runner.py
@@ -295,7 +295,7 @@ def run_tests(test_list, src_dir, build_dir, exeext, tmpdir, jobs=1, enable_cove
     #Set env vars
     if "BITCOIND" not in os.environ:
         os.environ["BITCOIND"] = build_dir + '/src/axed' + exeext
-        os.environ["BITCOINCLI"] = build_dir + '/src/bitcoin-cli' + exeext
+        os.environ["BITCOINCLI"] = build_dir + '/src/axe-cli' + exeext
 
     tests_dir = src_dir + '/test/functional/'
 