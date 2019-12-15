bytzck/BYTZ

Merge #11531: Check that new headers are not a descendant of an invalid block (more effeciently)
f3d4adf Make p2p-acceptablock not an extended test (Matt Corallo)
00dcda6 [qa] test that invalid blocks on an invalid chain get a disconnect (Matt Corallo)
015a525 Reject headers building on invalid chains by tracking invalidity (Matt Corallo)
932f118 Accept unrequested blocks with work equal to our tip (Matt Corallo)
3d9c70c Stop always storing blocks from whitelisted peers (Matt Corallo)
3b4ac43 Rewrite p2p-acceptblock in preparation for slight behavior changes (Matt Corallo)

Pull request description:

  @sdaftuar pointed out that the version in #11487 was somewhat DoS-able as someone could feed you a valid chain that forked off the the last checkpoint block and force you to do lots of work just walking backwards across blocks for each new block they gave you. We came up with a few proposals but settled on the one implemented here as likely the simplest without obvious DoS issues. It uses our existing on-load mapBlockIndex walk to make sure everything that descends from an invalid block is marked as such, and then simply caches blocks which we attempted to connect but which were found to be invalid. To avoid DoS issues during IBD, this will need to depend on #11458.

  Includes tests from #11487.

Tree-SHA512: 46aff8332908e122dae72ceb5fe8cd241902c2281a87f58a5fb486bf69d46458d84a096fdcb5f3e8e07fbcf7466232b10c429f4d67855425f11b38ac0bf612e1

test/functional/test_runner.py
@@ -136,6 +136,7 @@ BASE_SCRIPTS= [
     'uptime.py',
     'resendwallettransactions.py',
     'minchainwork.py',
+    'p2p-acceptblock.py', # NOTE: needs dash_hash to pass
 ]
 
 EXTENDED_SCRIPTS = [
@@ -163,7 +164,6 @@ EXTENDED_SCRIPTS = [
     'txindex.py',
     'forknotify.py',
     'invalidateblock.py',
-    'p2p-acceptblock.py', # NOTE: needs dash_hash to pass
 ]
 
 # Place EXTENDED_SCRIPTS first since it has the 3 longest running tests

AXErunners/axe

test/functional/test_runner.py
@@ -136,6 +136,7 @@ BASE_SCRIPTS= [
     'uptime.py',
     'resendwallettransactions.py',
     'minchainwork.py',
+    'p2p-acceptblock.py', # NOTE: needs axe_hash to pass
 ]
 
 EXTENDED_SCRIPTS = [
@@ -163,7 +164,6 @@ EXTENDED_SCRIPTS = [
     'txindex.py',
     'forknotify.py',
     'invalidateblock.py',
-    'p2p-acceptblock.py', # NOTE: needs axe_hash to pass
 ]
 
 # Place EXTENDED_SCRIPTS first since it has the 3 longest running tests