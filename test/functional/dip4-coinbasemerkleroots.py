dashpay/dash

Tests: Allow specifying different cmd-line params for each masternode

test/functional/dip4-coinbasemerkleroots.py
@@ -38,7 +38,7 @@ class TestNode(NodeConnCB):
 
 class LLMQCoinbaseCommitmentsTest(DashTestFramework):
     def set_test_params(self):
-        self.set_dash_test_params(6, 5, [], fast_dip3_enforcement=True)
+        self.set_dash_test_params(6, 5, fast_dip3_enforcement=True)
 
     def run_test(self):
         self.test_node = TestNode()

test/functional/llmq-chainlocks.py
@@ -18,7 +18,7 @@ Checks LLMQs based ChainLocks
 
 class LLMQChainLocksTest(DashTestFramework):
     def set_test_params(self):
-        self.set_dash_test_params(6, 5, [], fast_dip3_enforcement=True)
+        self.set_dash_test_params(6, 5, fast_dip3_enforcement=True)
 
     def run_test(self):
 

test/functional/llmq-dkgerrors.py
@@ -15,7 +15,7 @@ Simulate and check DKG errors
 
 class LLMQDKGErrors(DashTestFramework):
     def set_test_params(self):
-        self.set_dash_test_params(6, 5, ["-whitelist=127.0.0.1"], fast_dip3_enforcement=True)
+        self.set_dash_test_params(6, 5, [["-whitelist=127.0.0.1"]] * 6, fast_dip3_enforcement=True)
 
     def run_test(self):
 

test/functional/llmq-is-cl-conflicts.py
@@ -48,7 +48,7 @@ class TestNode(NodeConnCB):
 
 class LLMQ_IS_CL_Conflicts(DashTestFramework):
     def set_test_params(self):
-        self.set_dash_test_params(6, 5, [], fast_dip3_enforcement=True)
+        self.set_dash_test_params(6, 5, fast_dip3_enforcement=True)
         #disable_mocktime()
 
     def run_test(self):

test/functional/llmq-signing.py
@@ -18,7 +18,7 @@ Checks LLMQs signing sessions
 
 class LLMQSigningTest(DashTestFramework):
     def set_test_params(self):
-        self.set_dash_test_params(6, 5, [], fast_dip3_enforcement=True)
+        self.set_dash_test_params(6, 5, fast_dip3_enforcement=True)
 
     def run_test(self):
 

test/functional/llmq-simplepose.py
@@ -17,7 +17,7 @@ Checks simple PoSe system based on LLMQ commitments
 
 class LLMQSimplePoSeTest(DashTestFramework):
     def set_test_params(self):
-        self.set_dash_test_params(6, 5, [], fast_dip3_enforcement=True)
+        self.set_dash_test_params(6, 5, fast_dip3_enforcement=True)
 
     def run_test(self):
 

test/functional/p2p-instantsend.py
@@ -16,7 +16,7 @@ Tests InstantSend functionality (prevent doublespend for unconfirmed transaction
 
 class InstantSendTest(DashTestFramework):
     def set_test_params(self):
-        self.set_dash_test_params(9, 5, [], fast_dip3_enforcement=True)
+        self.set_dash_test_params(9, 5, fast_dip3_enforcement=True)
         # set sender,  receiver,  isolated nodes
         self.isolated_idx = 1
         self.receiver_idx = 2

test/functional/test_framework/test_framework.py
@@ -482,24 +482,26 @@ class MasternodeInfo:
 
 
 class DashTestFramework(BitcoinTestFramework):
-    def set_dash_test_params(self, num_nodes, masterodes_count, extra_args, fast_dip3_enforcement=False):
+    def set_dash_test_params(self, num_nodes, masterodes_count, extra_args=None, fast_dip3_enforcement=False):
         self.mn_count = masterodes_count
         self.num_nodes = num_nodes
         self.mninfo = []
         self.setup_clean_chain = True
         self.is_network_split = False
         # additional args
+        if extra_args is None:
+            extra_args = [[]] * num_nodes
+        assert_equal(len(extra_args), num_nodes)
         self.extra_args = extra_args
-
-        self.extra_args += ["-sporkkey=cP4EKFyJsHT39LDqgdcB43Y3YXjNyjb5Fuas1GQSeAtjnZWmZEQK"]
-
+        self.extra_args[0] += ["-sporkkey=cP4EKFyJsHT39LDqgdcB43Y3YXjNyjb5Fuas1GQSeAtjnZWmZEQK"]
         self.fast_dip3_enforcement = fast_dip3_enforcement
         if fast_dip3_enforcement:
-            self.extra_args += ["-dip3params=30:50"]
+            for i in range(0, num_nodes):
+                self.extra_args[i] += ["-dip3params=30:50"]
 
     def create_simple_node(self):
         idx = len(self.nodes)
-        self.add_nodes(1, extra_args=[self.extra_args])
+        self.add_nodes(1, extra_args=[self.extra_args[idx]])
         self.start_node(idx)
         for i in range(0, idx):
             connect_nodes(self.nodes[i], idx)
@@ -574,7 +576,7 @@ class DashTestFramework(BitcoinTestFramework):
         executor = ThreadPoolExecutor(max_workers=20)
 
         def do_start(idx):
-            args = ['-masternodeblsprivkey=%s' % self.mninfo[idx].keyOperator] + self.extra_args
+            args = ['-masternodeblsprivkey=%s' % self.mninfo[idx].keyOperator] + self.extra_args[idx + start_idx]
             self.start_node(idx + start_idx, extra_args=args)
             self.mninfo[idx].nodeIdx = idx + start_idx
             self.mninfo[idx].node = self.nodes[idx + start_idx]
@@ -608,7 +610,7 @@ class DashTestFramework(BitcoinTestFramework):
 
     def setup_network(self):
         self.log.info("Creating and starting controller node")
-        self.add_nodes(1, extra_args=[self.extra_args])
+        self.add_nodes(1, extra_args=[self.extra_args[0]])
         self.start_node(0)
         required_balance = MASTERNODE_COLLATERAL * self.mn_count + 1
         self.log.info("Generating %d coins" % required_balance)

AXErunners/axe

test/functional/dip4-coinbasemerkleroots.py
@@ -38,7 +38,7 @@ class TestNode(NodeConnCB):
 
 class LLMQCoinbaseCommitmentsTest(AxeTestFramework):
     def set_test_params(self):
-        self.set_axe_test_params(6, 5, [], fast_dip3_enforcement=True)
+        self.set_axe_test_params(6, 5, fast_dip3_enforcement=True)
 
     def run_test(self):
         self.test_node = TestNode()

test/functional/llmq-chainlocks.py
@@ -18,7 +18,7 @@ Checks LLMQs based ChainLocks
 
 class LLMQChainLocksTest(AxeTestFramework):
     def set_test_params(self):
-        self.set_axe_test_params(6, 5, [], fast_dip3_enforcement=True)
+        self.set_axe_test_params(6, 5, fast_dip3_enforcement=True)
 
     def run_test(self):
 

test/functional/llmq-dkgerrors.py
@@ -15,7 +15,7 @@ Simulate and check DKG errors
 
 class LLMQDKGErrors(AxeTestFramework):
     def set_test_params(self):
-        self.set_axe_test_params(6, 5, ["-whitelist=127.0.0.1"], fast_dip3_enforcement=True)
+        self.set_axe_test_params(6, 5, [["-whitelist=127.0.0.1"]] * 6, fast_dip3_enforcement=True)
 
     def run_test(self):
 

test/functional/llmq-is-cl-conflicts.py
@@ -48,7 +48,7 @@ class TestNode(NodeConnCB):
 
 class LLMQ_IS_CL_Conflicts(AxeTestFramework):
     def set_test_params(self):
-        self.set_axe_test_params(6, 5, [], fast_dip3_enforcement=True)
+        self.set_axe_test_params(6, 5, fast_dip3_enforcement=True)
         #disable_mocktime()
 
     def run_test(self):

test/functional/llmq-signing.py
@@ -18,7 +18,7 @@ Checks LLMQs signing sessions
 
 class LLMQSigningTest(AxeTestFramework):
     def set_test_params(self):
-        self.set_axe_test_params(6, 5, [], fast_dip3_enforcement=True)
+        self.set_axe_test_params(6, 5, fast_dip3_enforcement=True)
 
     def run_test(self):
 

test/functional/llmq-simplepose.py
@@ -17,7 +17,7 @@ Checks simple PoSe system based on LLMQ commitments
 
 class LLMQSimplePoSeTest(AxeTestFramework):
     def set_test_params(self):
-        self.set_axe_test_params(6, 5, [], fast_dip3_enforcement=True)
+        self.set_axe_test_params(6, 5, fast_dip3_enforcement=True)
 
     def run_test(self):
 

test/functional/p2p-instantsend.py
@@ -16,7 +16,7 @@ Tests InstantSend functionality (prevent doublespend for unconfirmed transaction
 
 class InstantSendTest(AxeTestFramework):
     def set_test_params(self):
-        self.set_axe_test_params(9, 5, [], fast_dip3_enforcement=True)
+        self.set_axe_test_params(9, 5, fast_dip3_enforcement=True)
         # set sender,  receiver,  isolated nodes
         self.isolated_idx = 1
         self.receiver_idx = 2

test/functional/test_framework/test_framework.py
@@ -481,25 +481,27 @@ class MasternodeInfo:
         self.collateral_vout = collateral_vout
 
 
-class AxeTestFramework(BitcoinTestFramework):
-    def set_axe_test_params(self, num_nodes, masterodes_count, extra_args, fast_dip3_enforcement=False):
+class DashTestFramework(BitcoinTestFramework):
+    def set_axe_test_params(self, num_nodes, masterodes_count, extra_args=None, fast_dip3_enforcement=False):
         self.mn_count = masterodes_count
         self.num_nodes = num_nodes
         self.mninfo = []
         self.setup_clean_chain = True
         self.is_network_split = False
         # additional args
+        if extra_args is None:
+            extra_args = [[]] * num_nodes
+        assert_equal(len(extra_args), num_nodes)
         self.extra_args = extra_args
-
-        self.extra_args += ["-sporkkey=cP4EKFyJsHT39LDqgdcB43Y3YXjNyjb5Fuas1GQSeAtjnZWmZEQK"]
-
+        self.extra_args[0] += ["-sporkkey=cP4EKFyJsHT39LDqgdcB43Y3YXjNyjb5Fuas1GQSeAtjnZWmZEQK"]
         self.fast_dip3_enforcement = fast_dip3_enforcement
         if fast_dip3_enforcement:
-            self.extra_args += ["-dip3params=30:50"]
+            for i in range(0, num_nodes):
+                self.extra_args[i] += ["-dip3params=30:50"]
 
     def create_simple_node(self):
         idx = len(self.nodes)
-        self.add_nodes(1, extra_args=[self.extra_args])
+        self.add_nodes(1, extra_args=[self.extra_args[idx]])
         self.start_node(idx)
         for i in range(0, idx):
             connect_nodes(self.nodes[i], idx)
@@ -574,7 +576,7 @@ class AxeTestFramework(BitcoinTestFramework):
         executor = ThreadPoolExecutor(max_workers=20)
 
         def do_start(idx):
-            args = ['-masternodeblsprivkey=%s' % self.mninfo[idx].keyOperator] + self.extra_args
+            args = ['-masternodeblsprivkey=%s' % self.mninfo[idx].keyOperator] + self.extra_args[idx + start_idx]
             self.start_node(idx + start_idx, extra_args=args)
             self.mninfo[idx].nodeIdx = idx + start_idx
             self.mninfo[idx].node = self.nodes[idx + start_idx]
@@ -608,7 +610,7 @@ class AxeTestFramework(BitcoinTestFramework):
 
     def setup_network(self):
         self.log.info("Creating and starting controller node")
-        self.add_nodes(1, extra_args=[self.extra_args])
+        self.add_nodes(1, extra_args=[self.extra_args[0]])
         self.start_node(0)
         required_balance = MASTERNODE_COLLATERAL * self.mn_count + 1
         self.log.info("Generating %d coins" % required_balance)