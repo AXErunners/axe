863526753b4d29b63066a8863b4622811576bf60
sarawut <sanit.sa@outlook.com>
2020-01-31 13:26:13 +1000

    fix test

--- src/spork.cpp
+++ src/spork.cpp
@@ -24,7 +24,7 @@ std::vector<CSporkDef> sporkDefs = {
     MAKE_SPORK_DEF(SPORK_15_DETERMINISTIC_MNS_ENABLED,     0),             // ON
     MAKE_SPORK_DEF(SPORK_16_INSTANTSEND_AUTOLOCKS,         0),             // ON
     MAKE_SPORK_DEF(SPORK_17_QUORUM_DKG_ENABLED,            0),             // ON
-    MAKE_SPORK_DEF(SPORK_19_CHAINLOCKS_ENABLED,            0),             // ON
+    MAKE_SPORK_DEF(SPORK_19_CHAINLOCKS_ENABLED,            368964),        // ON
     MAKE_SPORK_DEF(SPORK_20_INSTANTSEND_LLMQ_BASED,        0),             // ON
 };
 
@@ -223,7 +223,7 @@ bool CSporkManager::UpdateSpork(SporkId nSporkID, int64_t nValue, CConnman& conn
 bool CSporkManager::IsSporkActive(SporkId nSporkID)
 {
     int64_t nSporkValue = GetSporkValue(nSporkID);
-    if(nSporkID == SPORK_19_CHAINLOCKS_ENABLED && nSporkValue == 0 && (Params().NetworkIDString() == CBaseChainParams::REGTEST || Params().NetworkIDString() == CBaseChainParams::DEVNET))
+    if(nSporkID == SPORK_19_CHAINLOCKS_ENABLED && nSporkValue == 368964 && (Params().NetworkIDString() == CBaseChainParams::REGTEST || Params().NetworkIDString() == CBaseChainParams::DEVNET))
         return false;
     return nSporkValue < GetAdjustedTime();
 }
