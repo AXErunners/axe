bytzck/BYTZ

Use smaller LLMQs in regtest (#3269)
* Rename LLMQ_5_60 to LLMQ_TEST

* Introduce -llmqtestparams which allows to modify LLMQ_TEST on regtest

Also add support in DashTestFramework

* Use parameters size=3, threshold=2 as default for LLMQ_TEST

And fall back to the old parameters where necessary

* Wait for all contributions, even when one member is lying

Otherwise we might end up continuing too fast, which would fail the DKG.

* Update src/chainparams.cpp

Co-Authored-By: UdjinM6 <UdjinM6@users.noreply.github.com>

Co-authored-by: UdjinM6 <UdjinM6@users.noreply.github.com>

test/functional/dip4-coinbasemerkleroots.py
@@ -38,7 +38,7 @@ class TestNode(NodeConnCB):
 
 class LLMQCoinbaseCommitmentsTest(DashTestFramework):
     def set_test_params(self):
-        self.set_dash_test_params(6, 5, fast_dip3_enforcement=True)
+        self.set_dash_test_params(4, 3, fast_dip3_enforcement=True)
         self.set_dash_dip8_activation(200)
 
     def run_test(self):

test/functional/llmq-chainlocks.py
@@ -18,7 +18,7 @@ Checks LLMQs based ChainLocks
 
 class LLMQChainLocksTest(DashTestFramework):
     def set_test_params(self):
-        self.set_dash_test_params(6, 5, fast_dip3_enforcement=True)
+        self.set_dash_test_params(4, 3, fast_dip3_enforcement=True)
         self.set_dash_dip8_activation(10)
 
     def run_test(self):

test/functional/llmq-dkgerrors.py
@@ -15,7 +15,7 @@ Simulate and check DKG errors
 
 class LLMQDKGErrors(DashTestFramework):
     def set_test_params(self):
-        self.set_dash_test_params(6, 5, [["-whitelist=127.0.0.1"]] * 6, fast_dip3_enforcement=True)
+        self.set_dash_test_params(4, 3, [["-whitelist=127.0.0.1"]] * 4, fast_dip3_enforcement=True)
         self.set_dash_dip8_activation(10)
 
     def run_test(self):
@@ -33,18 +33,18 @@ class LLMQDKGErrors(DashTestFramework):
 
         # Lets omit the contribution
         self.mninfo[0].node.quorum('dkgsimerror', 'contribution-omit', '1')
-        qh = self.mine_quorum(expected_contributions=4)
+        qh = self.mine_quorum(expected_contributions=2)
         self.assert_member_valid(qh, self.mninfo[0].proTxHash, False)
 
         # Lets lie in the contribution but provide a correct justification
         self.mninfo[0].node.quorum('dkgsimerror', 'contribution-omit', '0')
         self.mninfo[0].node.quorum('dkgsimerror', 'contribution-lie', '1')
-        qh = self.mine_quorum(expected_contributions=5, expected_complaints=4, expected_justifications=1)
+        qh = self.mine_quorum(expected_contributions=3, expected_complaints=2, expected_justifications=1)
         self.assert_member_valid(qh, self.mninfo[0].proTxHash, True)
 
         # Lets lie in the contribution and then omit the justification
         self.mninfo[0].node.quorum('dkgsimerror', 'justify-omit', '1')
-        qh = self.mine_quorum(expected_contributions=4, expected_complaints=4)
+        qh = self.mine_quorum(expected_contributions=3, expected_complaints=2)
         self.assert_member_valid(qh, self.mninfo[0].proTxHash, False)
 
         # Heal some damage (don't get PoSe banned)
@@ -53,29 +53,26 @@ class LLMQDKGErrors(DashTestFramework):
         # Lets lie in the contribution and then also lie in the justification
         self.mninfo[0].node.quorum('dkgsimerror', 'justify-omit', '0')
         self.mninfo[0].node.quorum('dkgsimerror', 'justify-lie', '1')
-        qh = self.mine_quorum(expected_contributions=4, expected_complaints=4, expected_justifications=1)
+        qh = self.mine_quorum(expected_contributions=3, expected_complaints=2, expected_justifications=1)
         self.assert_member_valid(qh, self.mninfo[0].proTxHash, False)
 
         # Lets lie about another MN
         self.mninfo[0].node.quorum('dkgsimerror', 'contribution-lie', '0')
         self.mninfo[0].node.quorum('dkgsimerror', 'justify-lie', '0')
         self.mninfo[0].node.quorum('dkgsimerror', 'complain-lie', '1')
-        qh = self.mine_quorum(expected_contributions=5, expected_complaints=1, expected_justifications=4)
+        qh = self.mine_quorum(expected_contributions=3, expected_complaints=1, expected_justifications=2)
         self.assert_member_valid(qh, self.mninfo[0].proTxHash, True)
 
-        # Lets omit 2 premature commitments
+        # Lets omit 1 premature commitments
         self.mninfo[0].node.quorum('dkgsimerror', 'complain-lie', '0')
         self.mninfo[0].node.quorum('dkgsimerror', 'commit-omit', '1')
-        self.mninfo[1].node.quorum('dkgsimerror', 'commit-omit', '1')
-        qh = self.mine_quorum(expected_contributions=5, expected_complaints=0, expected_justifications=0, expected_commitments=3)
+        qh = self.mine_quorum(expected_contributions=3, expected_complaints=0, expected_justifications=0, expected_commitments=2)
         self.assert_member_valid(qh, self.mninfo[0].proTxHash, True)
 
-        # Lets lie in 2 premature commitments
+        # Lets lie in 1 premature commitments
         self.mninfo[0].node.quorum('dkgsimerror', 'commit-omit', '0')
-        self.mninfo[1].node.quorum('dkgsimerror', 'commit-omit', '0')
         self.mninfo[0].node.quorum('dkgsimerror', 'commit-lie', '1')
-        self.mninfo[1].node.quorum('dkgsimerror', 'commit-lie', '1')
-        qh = self.mine_quorum(expected_contributions=5, expected_complaints=0, expected_justifications=0, expected_commitments=3)
+        qh = self.mine_quorum(expected_contributions=3, expected_complaints=0, expected_justifications=0, expected_commitments=2)
         self.assert_member_valid(qh, self.mninfo[0].proTxHash, True)
 
     def assert_member_valid(self, quorumHash, proTxHash, expectedValid):

test/functional/llmq-is-cl-conflicts.py
@@ -48,7 +48,7 @@ class TestNode(NodeConnCB):
 
 class LLMQ_IS_CL_Conflicts(DashTestFramework):
     def set_test_params(self):
-        self.set_dash_test_params(6, 5, fast_dip3_enforcement=True)
+        self.set_dash_test_params(4, 3, fast_dip3_enforcement=True)
         self.set_dash_dip8_activation(10)
         #disable_mocktime()
 
@@ -172,7 +172,7 @@ class LLMQ_IS_CL_Conflicts(DashTestFramework):
         islock = self.create_islock(rawtx2)
 
         # Stop enough MNs so that ChainLocks don't work anymore
-        for i in range(3):
+        for i in range(2):
             self.stop_node(len(self.nodes) - 1)
             self.nodes.pop(len(self.nodes) - 1)
             self.mninfo.pop(len(self.mninfo) - 1)

test/functional/p2p-instantsend.py
@@ -16,7 +16,7 @@ Tests InstantSend functionality (prevent doublespend for unconfirmed transaction
 
 class InstantSendTest(DashTestFramework):
     def set_test_params(self):
-        self.set_dash_test_params(9, 5, fast_dip3_enforcement=True)
+        self.set_dash_test_params(7, 3, fast_dip3_enforcement=True)
         # set sender,  receiver,  isolated nodes
         self.isolated_idx = 1
         self.receiver_idx = 2

test/functional/test_framework/test_framework.py
@@ -499,11 +499,21 @@ class DashTestFramework(BitcoinTestFramework):
             for i in range(0, num_nodes):
                 self.extra_args[i].append("-dip3params=30:50")
 
+        # LLMQ default test params (no need to pass -llmqtestparams)
+        self.llmq_size = 3
+        self.llmq_threshold = 2
+
     def set_dash_dip8_activation(self, activate_after_block):
         window = int((activate_after_block + 2) / 3)
         threshold = int((window + 1) / 2)
         for i in range(0, self.num_nodes):
             self.extra_args[i].append("-vbparams=dip0008:0:999999999999:%d:%d" % (window, threshold))
+
+    def set_dash_llmq_test_params(self, llmq_size, llmq_threshold):
+        self.llmq_size = llmq_size
+        self.llmq_threshold = llmq_threshold
+        for i in range(0, self.num_nodes):
+            self.extra_args[i].append("-llmqtestparams=%d:%d" % (self.llmq_size, self.llmq_threshold))
 
     def create_simple_node(self):
         idx = len(self.nodes)
@@ -746,15 +756,16 @@ class DashTestFramework(BitcoinTestFramework):
             return all(node.spork('show') == sporks for node in self.nodes[1:])
         wait_until(check_sporks_same, timeout=timeout, sleep=0.5)
 
-    def wait_for_quorum_phase(self, phase, check_received_messages, check_received_messages_count, timeout=30):
+    def wait_for_quorum_phase(self, phase, expected_member_count, check_received_messages, check_received_messages_count, timeout=30):
         def check_dkg_session():
             all_ok = True
+            member_count = 0
             for mn in self.mninfo:
                 s = mn.node.quorum("dkgstatus")["session"]
-                if "llmq_5_60" not in s:
-                    all_ok = False
-                    break
-                s = s["llmq_5_60"]
+                if "llmq_test" not in s:
+                    continue
+                member_count += 1
+                s = s["llmq_test"]
                 if "phase" not in s:
                     all_ok = False
                     break
@@ -765,6 +776,8 @@ class DashTestFramework(BitcoinTestFramework):
                     if s[check_received_messages] < check_received_messages_count:
                         all_ok = False
                         break
+            if all_ok and member_count != expected_member_count:
+                return False
             return all_ok
         wait_until(check_dkg_session, timeout=timeout, sleep=0.1)
 
@@ -777,15 +790,22 @@ class DashTestFramework(BitcoinTestFramework):
                     all_ok = False
                     break
                 s = s["minableCommitments"]
-                if "llmq_5_60" not in s:
+                if "llmq_test" not in s:
                     all_ok = False
                     break
             return all_ok
         wait_until(check_dkg_comitments, timeout=timeout, sleep=0.1)
 
-    def mine_quorum(self, expected_contributions=5, expected_complaints=0, expected_justifications=0, expected_commitments=5):
-        self.log.info("Mining quorum: expected_contributions=%d, expected_complaints=%d, expected_justifications=%d, "
-                      "expected_commitments=%d" % (expected_contributions, expected_complaints,
+    def mine_quorum(self, expected_members=None, expected_contributions=None, expected_complaints=0, expected_justifications=0, expected_commitments=None):
+        if expected_members is None:
+            expected_members = self.llmq_size
+        if expected_contributions is None:
+            expected_contributions = self.llmq_size
+        if expected_commitments is None:
+            expected_commitments = self.llmq_size
+
+        self.log.info("Mining quorum: expected_members=%d, expected_contributions=%d, expected_complaints=%d, expected_justifications=%d, "
+                      "expected_commitments=%d" % (expected_members, expected_contributions, expected_complaints,
                                                    expected_justifications, expected_commitments))
 
         quorums = self.nodes[0].quorum("list")
@@ -799,7 +819,7 @@ class DashTestFramework(BitcoinTestFramework):
         sync_blocks(self.nodes)
 
         self.log.info("Waiting for phase 1 (init)")
-        self.wait_for_quorum_phase(1, None, 0)
+        self.wait_for_quorum_phase(1, expected_members, None, 0)
         # Give nodes some time to connect to neighbors
         time.sleep(2)
         self.bump_mocktime(1)
@@ -808,35 +828,35 @@ class DashTestFramework(BitcoinTestFramework):
         sync_blocks(self.nodes)
 
         self.log.info("Waiting for phase 2 (contribute)")
-        self.wait_for_quorum_phase(2, "receivedContributions", expected_contributions)
+        self.wait_for_quorum_phase(2, expected_members, "receivedContributions", expected_contributions)
         self.bump_mocktime(1)
         set_node_times(self.nodes, self.mocktime)
         self.nodes[0].generate(2)
         sync_blocks(self.nodes)
 
         self.log.info("Waiting for phase 3 (complain)")
-        self.wait_for_quorum_phase(3, "receivedComplaints", expected_complaints)
+        self.wait_for_quorum_phase(3, expected_members, "receivedComplaints", expected_complaints)
         self.bump_mocktime(1)
         set_node_times(self.nodes, self.mocktime)
         self.nodes[0].generate(2)
         sync_blocks(self.nodes)
 
         self.log.info("Waiting for phase 4 (justify)")
-        self.wait_for_quorum_phase(4, "receivedJustifications", expected_justifications)
+        self.wait_for_quorum_phase(4, expected_members, "receivedJustifications", expected_justifications)
         self.bump_mocktime(1)
         set_node_times(self.nodes, self.mocktime)
         self.nodes[0].generate(2)
         sync_blocks(self.nodes)
 
         self.log.info("Waiting for phase 5 (commit)")
-        self.wait_for_quorum_phase(5, "receivedPrematureCommitments", expected_commitments)
+        self.wait_for_quorum_phase(5, expected_members, "receivedPrematureCommitments", expected_commitments)
         self.bump_mocktime(1)
         set_node_times(self.nodes, self.mocktime)
         self.nodes[0].generate(2)
         sync_blocks(self.nodes)
 
         self.log.info("Waiting for phase 6 (mining)")
-        self.wait_for_quorum_phase(6, None, 0)
+        self.wait_for_quorum_phase(6, expected_members, None, 0)
 
         self.log.info("Waiting final commitment")
         self.wait_for_quorum_commitment()
@@ -851,7 +871,7 @@ class DashTestFramework(BitcoinTestFramework):
             set_node_times(self.nodes, self.mocktime)
             self.nodes[0].generate(1)
             sync_blocks(self.nodes)
-        new_quorum = self.nodes[0].quorum("list", 1)["llmq_5_60"][0]
+        new_quorum = self.nodes[0].quorum("list", 1)["llmq_test"][0]
         quorum_info = self.nodes[0].quorum("info", 100, new_quorum)
 
         # Mine 8 (SIGN_HEIGHT_OFFSET) more blocks to make sure that the new quorum gets eligable for signing sessions

AXErunners/axe

test/functional/dip4-coinbasemerkleroots.py
@@ -38,7 +38,7 @@ class TestNode(NodeConnCB):
 
 class LLMQCoinbaseCommitmentsTest(AxeTestFramework):
     def set_test_params(self):
-        self.set_axe_test_params(6, 5, fast_dip3_enforcement=True)
+        self.set_axe_test_params(4, 3, fast_dip3_enforcement=True)
         self.set_axe_dip8_activation(200)
 
     def run_test(self):

test/functional/llmq-chainlocks.py
@@ -18,7 +18,7 @@ Checks LLMQs based ChainLocks
 
 class LLMQChainLocksTest(AxeTestFramework):
     def set_test_params(self):
-        self.set_axe_test_params(6, 5, fast_dip3_enforcement=True)
+        self.set_axe_test_params(4, 3, fast_dip3_enforcement=True)
         self.set_axe_dip8_activation(10)
 
     def run_test(self):

test/functional/llmq-dkgerrors.py
@@ -15,7 +15,7 @@ Simulate and check DKG errors
 
 class LLMQDKGErrors(AxeTestFramework):
     def set_test_params(self):
-        self.set_axe_test_params(6, 5, [["-whitelist=127.0.0.1"]] * 6, fast_dip3_enforcement=True)
+        self.set_axe_test_params(4, 3, [["-whitelist=127.0.0.1"]] * 4, fast_dip3_enforcement=True)
         self.set_axe_dip8_activation(10)
 
     def run_test(self):
@@ -33,18 +33,18 @@ class LLMQDKGErrors(AxeTestFramework):
 
         # Lets omit the contribution
         self.mninfo[0].node.quorum('dkgsimerror', 'contribution-omit', '1')
-        qh = self.mine_quorum(expected_contributions=4)
+        qh = self.mine_quorum(expected_contributions=2)
         self.assert_member_valid(qh, self.mninfo[0].proTxHash, False)
 
         # Lets lie in the contribution but provide a correct justification
         self.mninfo[0].node.quorum('dkgsimerror', 'contribution-omit', '0')
         self.mninfo[0].node.quorum('dkgsimerror', 'contribution-lie', '1')
-        qh = self.mine_quorum(expected_contributions=5, expected_complaints=4, expected_justifications=1)
+        qh = self.mine_quorum(expected_contributions=3, expected_complaints=2, expected_justifications=1)
         self.assert_member_valid(qh, self.mninfo[0].proTxHash, True)
 
         # Lets lie in the contribution and then omit the justification
         self.mninfo[0].node.quorum('dkgsimerror', 'justify-omit', '1')
-        qh = self.mine_quorum(expected_contributions=4, expected_complaints=4)
+        qh = self.mine_quorum(expected_contributions=3, expected_complaints=2)
         self.assert_member_valid(qh, self.mninfo[0].proTxHash, False)
 
         # Heal some damage (don't get PoSe banned)
@@ -53,29 +53,26 @@ class LLMQDKGErrors(AxeTestFramework):
         # Lets lie in the contribution and then also lie in the justification
         self.mninfo[0].node.quorum('dkgsimerror', 'justify-omit', '0')
         self.mninfo[0].node.quorum('dkgsimerror', 'justify-lie', '1')
-        qh = self.mine_quorum(expected_contributions=4, expected_complaints=4, expected_justifications=1)
+        qh = self.mine_quorum(expected_contributions=3, expected_complaints=2, expected_justifications=1)
         self.assert_member_valid(qh, self.mninfo[0].proTxHash, False)
 
         # Lets lie about another MN
         self.mninfo[0].node.quorum('dkgsimerror', 'contribution-lie', '0')
         self.mninfo[0].node.quorum('dkgsimerror', 'justify-lie', '0')
         self.mninfo[0].node.quorum('dkgsimerror', 'complain-lie', '1')
-        qh = self.mine_quorum(expected_contributions=5, expected_complaints=1, expected_justifications=4)
+        qh = self.mine_quorum(expected_contributions=3, expected_complaints=1, expected_justifications=2)
         self.assert_member_valid(qh, self.mninfo[0].proTxHash, True)
 
-        # Lets omit 2 premature commitments
+        # Lets omit 1 premature commitments
         self.mninfo[0].node.quorum('dkgsimerror', 'complain-lie', '0')
         self.mninfo[0].node.quorum('dkgsimerror', 'commit-omit', '1')
-        self.mninfo[1].node.quorum('dkgsimerror', 'commit-omit', '1')
-        qh = self.mine_quorum(expected_contributions=5, expected_complaints=0, expected_justifications=0, expected_commitments=3)
+        qh = self.mine_quorum(expected_contributions=3, expected_complaints=0, expected_justifications=0, expected_commitments=2)
         self.assert_member_valid(qh, self.mninfo[0].proTxHash, True)
 
-        # Lets lie in 2 premature commitments
+        # Lets lie in 1 premature commitments
         self.mninfo[0].node.quorum('dkgsimerror', 'commit-omit', '0')
-        self.mninfo[1].node.quorum('dkgsimerror', 'commit-omit', '0')
         self.mninfo[0].node.quorum('dkgsimerror', 'commit-lie', '1')
-        self.mninfo[1].node.quorum('dkgsimerror', 'commit-lie', '1')
-        qh = self.mine_quorum(expected_contributions=5, expected_complaints=0, expected_justifications=0, expected_commitments=3)
+        qh = self.mine_quorum(expected_contributions=3, expected_complaints=0, expected_justifications=0, expected_commitments=2)
         self.assert_member_valid(qh, self.mninfo[0].proTxHash, True)
 
     def assert_member_valid(self, quorumHash, proTxHash, expectedValid):

test/functional/llmq-is-cl-conflicts.py
@@ -48,7 +48,7 @@ class TestNode(NodeConnCB):
 
 class LLMQ_IS_CL_Conflicts(AxeTestFramework):
     def set_test_params(self):
-        self.set_axe_test_params(6, 5, fast_dip3_enforcement=True)
+        self.set_axe_test_params(4, 3, fast_dip3_enforcement=True)
         self.set_axe_dip8_activation(10)
         #disable_mocktime()
 
@@ -172,7 +172,7 @@ class LLMQ_IS_CL_Conflicts(AxeTestFramework):
         islock = self.create_islock(rawtx2)
 
         # Stop enough MNs so that ChainLocks don't work anymore
-        for i in range(3):
+        for i in range(2):
             self.stop_node(len(self.nodes) - 1)
             self.nodes.pop(len(self.nodes) - 1)
             self.mninfo.pop(len(self.mninfo) - 1)

test/functional/p2p-instantsend.py
@@ -16,7 +16,7 @@ Tests InstantSend functionality (prevent doublespend for unconfirmed transaction
 
 class InstantSendTest(AxeTestFramework):
     def set_test_params(self):
-        self.set_axe_test_params(9, 5, fast_dip3_enforcement=True)
+        self.set_axe_test_params(7, 3, fast_dip3_enforcement=True)
         # set sender,  receiver,  isolated nodes
         self.isolated_idx = 1
         self.receiver_idx = 2

test/functional/test_framework/test_framework.py
@@ -499,11 +499,21 @@ class AxeTestFramework(BitcoinTestFramework):
             for i in range(0, num_nodes):
                 self.extra_args[i].append("-dip3params=30:50")
 
+        # LLMQ default test params (no need to pass -llmqtestparams)
+        self.llmq_size = 3
+        self.llmq_threshold = 2
+
     def set_axe_dip8_activation(self, activate_after_block):
         window = int((activate_after_block + 2) / 3)
         threshold = int((window + 1) / 2)
         for i in range(0, self.num_nodes):
             self.extra_args[i].append("-vbparams=dip0008:0:999999999999:%d:%d" % (window, threshold))
+
+    def set_axe_llmq_test_params(self, llmq_size, llmq_threshold):
+        self.llmq_size = llmq_size
+        self.llmq_threshold = llmq_threshold
+        for i in range(0, self.num_nodes):
+            self.extra_args[i].append("-llmqtestparams=%d:%d" % (self.llmq_size, self.llmq_threshold))
 
     def create_simple_node(self):
         idx = len(self.nodes)
@@ -746,15 +756,16 @@ class AxeTestFramework(BitcoinTestFramework):
             return all(node.spork('show') == sporks for node in self.nodes[1:])
         wait_until(check_sporks_same, timeout=timeout, sleep=0.5)
 
-    def wait_for_quorum_phase(self, phase, check_received_messages, check_received_messages_count, timeout=30):
+    def wait_for_quorum_phase(self, phase, expected_member_count, check_received_messages, check_received_messages_count, timeout=30):
         def check_dkg_session():
             all_ok = True
+            member_count = 0
             for mn in self.mninfo:
                 s = mn.node.quorum("dkgstatus")["session"]
-                if "llmq_5_60" not in s:
-                    all_ok = False
-                    break
-                s = s["llmq_5_60"]
+                if "llmq_test" not in s:
+                    continue
+                member_count += 1
+                s = s["llmq_test"]
                 if "phase" not in s:
                     all_ok = False
                     break
@@ -765,6 +776,8 @@ class AxeTestFramework(BitcoinTestFramework):
                     if s[check_received_messages] < check_received_messages_count:
                         all_ok = False
                         break
+            if all_ok and member_count != expected_member_count:
+                return False
             return all_ok
         wait_until(check_dkg_session, timeout=timeout, sleep=0.1)
 
@@ -777,15 +790,22 @@ class AxeTestFramework(BitcoinTestFramework):
                     all_ok = False
                     break
                 s = s["minableCommitments"]
-                if "llmq_5_60" not in s:
+                if "llmq_test" not in s:
                     all_ok = False
                     break
             return all_ok
         wait_until(check_dkg_comitments, timeout=timeout, sleep=0.1)
 
-    def mine_quorum(self, expected_contributions=5, expected_complaints=0, expected_justifications=0, expected_commitments=5):
-        self.log.info("Mining quorum: expected_contributions=%d, expected_complaints=%d, expected_justifications=%d, "
-                      "expected_commitments=%d" % (expected_contributions, expected_complaints,
+    def mine_quorum(self, expected_members=None, expected_contributions=None, expected_complaints=0, expected_justifications=0, expected_commitments=None):
+        if expected_members is None:
+            expected_members = self.llmq_size
+        if expected_contributions is None:
+            expected_contributions = self.llmq_size
+        if expected_commitments is None:
+            expected_commitments = self.llmq_size
+
+        self.log.info("Mining quorum: expected_members=%d, expected_contributions=%d, expected_complaints=%d, expected_justifications=%d, "
+                      "expected_commitments=%d" % (expected_members, expected_contributions, expected_complaints,
                                                    expected_justifications, expected_commitments))
 
         quorums = self.nodes[0].quorum("list")
@@ -799,7 +819,7 @@ class AxeTestFramework(BitcoinTestFramework):
         sync_blocks(self.nodes)
 
         self.log.info("Waiting for phase 1 (init)")
-        self.wait_for_quorum_phase(1, None, 0)
+        self.wait_for_quorum_phase(1, expected_members, None, 0)
         # Give nodes some time to connect to neighbors
         time.sleep(2)
         self.bump_mocktime(1)
@@ -808,35 +828,35 @@ class AxeTestFramework(BitcoinTestFramework):
         sync_blocks(self.nodes)
 
         self.log.info("Waiting for phase 2 (contribute)")
-        self.wait_for_quorum_phase(2, "receivedContributions", expected_contributions)
+        self.wait_for_quorum_phase(2, expected_members, "receivedContributions", expected_contributions)
         self.bump_mocktime(1)
         set_node_times(self.nodes, self.mocktime)
         self.nodes[0].generate(2)
         sync_blocks(self.nodes)
 
         self.log.info("Waiting for phase 3 (complain)")
-        self.wait_for_quorum_phase(3, "receivedComplaints", expected_complaints)
+        self.wait_for_quorum_phase(3, expected_members, "receivedComplaints", expected_complaints)
         self.bump_mocktime(1)
         set_node_times(self.nodes, self.mocktime)
         self.nodes[0].generate(2)
         sync_blocks(self.nodes)
 
         self.log.info("Waiting for phase 4 (justify)")
-        self.wait_for_quorum_phase(4, "receivedJustifications", expected_justifications)
+        self.wait_for_quorum_phase(4, expected_members, "receivedJustifications", expected_justifications)
         self.bump_mocktime(1)
         set_node_times(self.nodes, self.mocktime)
         self.nodes[0].generate(2)
         sync_blocks(self.nodes)
 
         self.log.info("Waiting for phase 5 (commit)")
-        self.wait_for_quorum_phase(5, "receivedPrematureCommitments", expected_commitments)
+        self.wait_for_quorum_phase(5, expected_members, "receivedPrematureCommitments", expected_commitments)
         self.bump_mocktime(1)
         set_node_times(self.nodes, self.mocktime)
         self.nodes[0].generate(2)
         sync_blocks(self.nodes)
 
         self.log.info("Waiting for phase 6 (mining)")
-        self.wait_for_quorum_phase(6, None, 0)
+        self.wait_for_quorum_phase(6, expected_members, None, 0)
 
         self.log.info("Waiting final commitment")
         self.wait_for_quorum_commitment()
@@ -851,7 +871,7 @@ class AxeTestFramework(BitcoinTestFramework):
             set_node_times(self.nodes, self.mocktime)
             self.nodes[0].generate(1)
             sync_blocks(self.nodes)
-        new_quorum = self.nodes[0].quorum("list", 1)["llmq_5_60"][0]
+        new_quorum = self.nodes[0].quorum("list", 1)["llmq_test"][0]
         quorum_info = self.nodes[0].quorum("info", 100, new_quorum)
 
         # Mine 8 (SIGN_HEIGHT_OFFSET) more blocks to make sure that the new quorum gets eligable for signing sessions