Bitcoin-ABC/bitcoin-abc

Merge #11351: Refactor: Modernize disallowed copy constructors/assignment
Summary:
2a07f878a Refactor: Modernize disallowed copy constructors/assignment (Dan Raviv)

Pull request description:

  Use C++11's better capability of expressing an interface of a non-copyable class by publicly deleting its copy ctor and assignment operator instead of just declaring them private.

Tree-SHA512: 878f446be5a136bb2a90643aaeaca62948b575e6ef71ccc5b4b8f373e66f36ced00665128f36504e0ccfee639863d969329c4276154ef9f2a9de9137f0801e01

Backport of Core PR11351
https://github.com/bitcoin/bitcoin/pull/11351

Test Plan:
make check
test_runner.py

Reviewers: jasonbcox, deadalnix, Fabien, O1 Bitcoin ABC, #bitcoin_abc, markblundeberg

Reviewed By: O1 Bitcoin ABC, #bitcoin_abc, markblundeberg

Subscribers: markblundeberg

Differential Revision: https://reviews.bitcoinabc.org/D2983

src/streams.h
@@ -572,10 +572,6 @@ public:
  */
 class CAutoFile {
 private:
-    // Disallow copies
-    CAutoFile(const CAutoFile &);
-    CAutoFile &operator=(const CAutoFile &);
-
     const int nType;
     const int nVersion;
 
@@ -588,6 +584,10 @@ public:
     }
 
     ~CAutoFile() { fclose(); }
+
+    // Disallow copies
+    CAutoFile(const CAutoFile &) = delete;
+    CAutoFile &operator=(const CAutoFile &) = delete;
 
     void fclose() {
         if (file) {
@@ -686,10 +686,6 @@ public:
  */
 class CBufferedFile {
 private:
-    // Disallow copies
-    CBufferedFile(const CBufferedFile &);
-    CBufferedFile &operator=(const CBufferedFile &);
-
     const int nType;
     const int nVersion;
 
@@ -734,6 +730,10 @@ public:
     }
 
     ~CBufferedFile() { fclose(); }
+
+    // Disallow copies
+    CBufferedFile(const CBufferedFile &) = delete;
+    CBufferedFile &operator=(const CBufferedFile &) = delete;
 
     int GetVersion() const { return nVersion; }
     int GetType() const { return nType; }

src/support/lockedpool.h
@@ -51,6 +51,9 @@ public:
     Arena(void *base, size_t size, size_t alignment);
     virtual ~Arena();
 
+    Arena(const Arena &other) = delete;       // non construction-copyable
+    Arena &operator=(const Arena &) = delete; // non copyable
+
     /** Memory statistics. */
     struct Stats {
         size_t used;
@@ -89,11 +92,6 @@ public:
     bool addressInArena(void *ptr) const { return ptr >= base && ptr < end; }
 
 private:
-    // non construction-copyable
-    Arena(const Arena &other) = delete;
-    // non copyable
-    Arena &operator=(const Arena &) = delete;
-
     /**
      * Map of chunk address to chunk information. This class makes use of the
      * sorted order to merge previous and next chunks during deallocation.
@@ -165,6 +163,9 @@ public:
                         LockingFailed_Callback lf_cb_in = nullptr);
     ~LockedPool();
 
+    LockedPool(const LockedPool &other) = delete; // non construction-copyable
+    LockedPool &operator=(const LockedPool &) = delete; // non copyable
+
     /**
      * Allocate size bytes from this arena.
      * Returns pointer on success, or 0 if memory is full or the application
@@ -183,11 +184,6 @@ public:
     Stats stats() const;
 
 private:
-    // non construction-copyable
-    LockedPool(const LockedPool &other) = delete;
-    // non copyable
-    LockedPool &operator=(const LockedPool &) = delete;
-
     std::unique_ptr<LockedPageAllocator> allocator;
 
     /** Create an arena from locked pages */

AXErunners/axe

src/streams.h
@@ -412,10 +412,6 @@ public:
 class CAutoFile
 {
 private:
-    // Disallow copies
-    CAutoFile(const CAutoFile&);
-    CAutoFile& operator=(const CAutoFile&);
-
     const int nType;
     const int nVersion;
 
@@ -431,6 +427,10 @@ public:
     {
         fclose();
     }
+
+    // Disallow copies
+    CAutoFile(const CAutoFile&) = delete;
+    CAutoFile& operator=(const CAutoFile&) = delete;
 
     void fclose()
     {
@@ -521,10 +521,6 @@ public:
 class CBufferedFile
 {
 private:
-    // Disallow copies
-    CBufferedFile(const CBufferedFile&);
-    CBufferedFile& operator=(const CBufferedFile&);
-
     const int nType;
     const int nVersion;
 
@@ -565,6 +561,10 @@ public:
     {
         fclose();
     }
+
+    // Disallow copies
+    CBufferedFile(const CBufferedFile&) = delete;
+    CBufferedFile& operator=(const CBufferedFile&) = delete;
 
     int GetVersion() const { return nVersion; }
     int GetType() const { return nType; }

src/support/lockedpool.h
@@ -50,6 +50,9 @@ public:
     Arena(void *base, size_t size, size_t alignment);
     virtual ~Arena();
 
+    Arena(const Arena& other) = delete; // non construction-copyable
+    Arena& operator=(const Arena&) = delete; // non copyable
+
     /** Memory statistics. */
     struct Stats
     {
@@ -85,9 +88,6 @@ public:
      */
     bool addressInArena(void *ptr) const { return ptr >= base && ptr < end; }
 private:
-    Arena(const Arena& other) = delete; // non construction-copyable
-    Arena& operator=(const Arena&) = delete; // non copyable
-
     /** Map of chunk address to chunk information. This class makes use of the
      * sorted order to merge previous and next chunks during deallocation.
      */
@@ -153,6 +153,9 @@ public:
     explicit LockedPool(std::unique_ptr<LockedPageAllocator> allocator, LockingFailed_Callback lf_cb_in = nullptr);
     ~LockedPool();
 
+    LockedPool(const LockedPool& other) = delete; // non construction-copyable
+    LockedPool& operator=(const LockedPool&) = delete; // non copyable
+
     /** Allocate size bytes from this arena.
      * Returns pointer on success, or 0 if memory is full or
      * the application tried to allocate 0 bytes.
@@ -168,9 +171,6 @@ public:
     /** Get pool usage statistics */
     Stats stats() const;
 private:
-    LockedPool(const LockedPool& other) = delete; // non construction-copyable
-    LockedPool& operator=(const LockedPool&) = delete; // non copyable
-
     std::unique_ptr<LockedPageAllocator> allocator;
 
     /** Create an arena from locked pages */