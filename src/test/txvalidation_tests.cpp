36785565fa7fdf6ddc68602d49e6aa778f6fbd95
-k <slowdive@me.com>
2020-03-24 23:22:33 -0700

    Fix txvalidation_tests

--- src/test/txvalidation_tests.cpp
+++ src/test/txvalidation_tests.cpp
@@ -8,7 +8,7 @@
 #include <consensus/validation.h>
 #include <primitives/transaction.h>
 #include <script/script.h>
-#include <test/test_dash.h>
+#include <test/test_axe.h>
 
 #include <boost/test/unit_test.hpp>
 
