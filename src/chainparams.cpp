fa472b596fa37c602a7311eb860d9ce9331b457b
Alexander Block <ablock84@gmail.com>
2020-03-31 07:16:35 +0200

    Fix fRequireRoutableExternalIP for devnets

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -741,6 +741,7 @@ public:
 
         fDefaultConsistencyChecks = false;
         fRequireStandard = false;
+        fRequireRoutableExternalIP = true;
         fMineBlocksOnDemand = false;
         fAllowMultipleAddressesFromGroup = true;
         fAllowMultiplePorts = true;
