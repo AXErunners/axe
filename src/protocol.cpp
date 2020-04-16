f63e65de7cf3fc7b63fd95f9fd195ae996cfe4c2
Pasta <pasta@dashboost.org>
2020-04-15 21:32:44 -0500

    remove witness
    Signed-off-by: Pasta <pasta@dashboost.org>

--- src/protocol.cpp
+++ src/protocol.cpp
@@ -200,9 +200,9 @@ bool CMessageHeader::IsValid(const MessageStartChars& pchMessageStartIn) const
 
 ServiceFlags GetDesirableServiceFlags(ServiceFlags services) {
     if ((services & NODE_NETWORK_LIMITED) && g_initial_block_download_completed) {
-        return ServiceFlags(NODE_NETWORK_LIMITED | NODE_WITNESS);
+        return ServiceFlags(NODE_NETWORK_LIMITED);
     }
-    return ServiceFlags(NODE_NETWORK | NODE_WITNESS);
+    return ServiceFlags(NODE_NETWORK);
 }
 
 void SetServiceFlagsIBDCache(bool state) {
