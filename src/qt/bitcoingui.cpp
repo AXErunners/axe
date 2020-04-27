dashpay/dash

review tab space and blue logo name

src/qt/bitcoingui.cpp
@@ -601,7 +601,7 @@ void BitcoinGUI::createToolBars()
         QString theme = settings.value("theme", "").toString();
         QString logoImage = ":/images/dash_logo_toolbar";
          if (theme == "Traditional") {
-           logoImage = ":/images/dash_logo_blue";
+             logoImage = ":/images/dash_logo_toolbar_blue";
          }
 
         QPixmap logoPixmap(logoImage);
@@ -678,13 +678,13 @@ void BitcoinGUI::setClientModel(ClientModel *_clientModel)
         }
 #endif // ENABLE_WALLET
         unitDisplayControl->setOptionsModel(_clientModel->getOptionsModel());
-        
+
         OptionsModel* optionsModel = _clientModel->getOptionsModel();
         if(optionsModel)
         {
             // be aware of the tray icon disable state change reported by the OptionsModel object.
             connect(optionsModel,SIGNAL(hideTrayIconChanged(bool)),this,SLOT(setTrayIconVisible(bool)));
-        
+
             // initialize the disable state of the tray icon with the current value in the model.
             setTrayIconVisible(optionsModel->getHideTrayIcon());
         }

AXErunners/axe

src/qt/bitcoingui.cpp
@@ -601,7 +601,7 @@ void BitcoinGUI::createToolBars()
         QString theme = settings.value("theme", "").toString();
         QString logoImage = ":/images/axe_logo_toolbar";
          if (theme == "Traditional") {
-           logoImage = ":/images/axe_logo_blue";
+             logoImage = ":/images/axe_logo_toolbar_blue";
          }
 
         QPixmap logoPixmap(logoImage);