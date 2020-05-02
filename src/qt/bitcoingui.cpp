hebasto/bitcoin-qtw

Remove obj_c for macOS Dock icon menu
Qt `setAsDockMenu()` does this work.

src/qt/bitcoingui.cpp
@@ -607,7 +607,9 @@ void BitcoinGUI::createTrayIconMenu()
     // Note: On macOS, the Dock icon is used to provide the tray's functionality.
     MacDockIconHandler *dockIconHandler = MacDockIconHandler::instance();
     connect(dockIconHandler, &MacDockIconHandler::dockIconClicked, this, &BitcoinGUI::macosDockIconActivated);
-    trayIconMenu = dockIconHandler->dockMenu();
+
+    trayIconMenu = new QMenu(this);
+    trayIconMenu->setAsDockMenu();
 #endif
 
     // Configuration of the tray icon (or Dock icon) menu

AXErunners/axe

src/qt/bitcoingui.cpp
@@ -642,7 +642,9 @@ void BitcoinGUI::setClientModel(ClientModel *_clientModel)
             // similar to one for tray icon
             MacDockIconHandler *dockIconHandler = MacDockIconHandler::instance();
             connect(dockIconHandler, SIGNAL(dockIconClicked()), this, SLOT(macosDockIconActivated()));
-            dockIconMenu = dockIconHandler->dockMenu();
+
+            dockIconMenu = new QMenu(this);
+            dockIconMenu->setAsDockMenu();
 
             createIconMenu(dockIconMenu);
 #endif