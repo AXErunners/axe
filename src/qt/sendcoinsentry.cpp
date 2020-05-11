632ae12c37cb7019555f98490d1ec62445b1cde2
xdustinface <xdustinfacex@gmail.com>
2020-05-11 19:14:02 +0200

    qt: Always prefer the addressbook's label over an URI's label

--- src/qt/sendcoinsentry.cpp
+++ src/qt/sendcoinsentry.cpp
@@ -83,9 +83,9 @@ void SendCoinsEntry::on_payTo_textChanged(const QString &address)
         ui->payTo->blockSignals(true);
         setValue(rcp);
         ui->payTo->blockSignals(false);
+    } else {
+        updateLabel(rcp.address);
     }
-
-    updateLabel(rcp.address);
 }
 
 void SendCoinsEntry::setModel(WalletModel *_model)
@@ -233,12 +233,12 @@ void SendCoinsEntry::setValue(const SendCoinsRecipient &value)
         ui->messageTextLabel->setVisible(!recipient.message.isEmpty());
         ui->messageLabel->setVisible(!recipient.message.isEmpty());
 
-        ui->addAsLabel->clear();
-        ui->payTo->setText(recipient.address); // this may set a label from addressbook
-        if (!recipient.label.isEmpty()) // if a label had been set from the addressbook, don't overwrite with an empty label
-            ui->addAsLabel->setText(recipient.label);
+        ui->payTo->setText(recipient.address);
+        ui->addAsLabel->setText(recipient.label);
         ui->payAmount->setValue(recipient.amount);
     }
+
+    updateLabel(recipient.address);
 }
 
 void SendCoinsEntry::setAddress(const QString &address)
