88b7b05fac1a64a67e9a393daaf56af785f221fe
-k <slowdive@me.com>
2020-06-01 14:16:04 -0700

    s/dash/axe

--- src/privatesend/privatesend-client.h
+++ src/privatesend/privatesend-client.h
@@ -42,7 +42,7 @@ static const bool DEFAULT_PRIVATESEND_MULTISESSION = false;
 // Knowing that each CTxOut is ~35b big, 400 outputs should take 400 x ~35b = ~17.5kb.
 // More than 500 outputs starts to make qt quite laggy.
 // Additionally to need all 500 outputs (assuming a max per denom of 50) you'd need to be trying to
-// create denominations for over 3000 dash!
+// create denominations for over 3000 axe!
 static const int PRIVATESEND_DENOM_OUTPUTS_THRESHOLD = 500;
 
 // Warn user if mixing in gui or try to create backup if mixing in daemon mode
