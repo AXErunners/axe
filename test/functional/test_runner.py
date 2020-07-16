605f73ea6f93067ad82c4f71f24850befb775b91
PastaPastaPasta <6443210+PastaPastaPasta@users.noreply.github.com>
2020-07-16 23:44:20 +0000

    Backport 11796 + 11774 (#3612)
    * Merge #11796: [tests] Functional test naming convention
    
    5fecd84 [tests] Remove redundant import in blocktools.py test (Anthony Towns)
    9b20bb4 [tests] Check tests conform to naming convention (Anthony Towns)
    7250b4e [tests] README.md nit fixes (Anthony Towns)
    82b2712 [tests] move witness util functions to blocktools.py (John Newbery)
    1e10854 [tests] [docs] update README for new test naming scheme (John Newbery)
    
    Pull request description:
    
      Splitting #11774 into two parts -- this part updates the README with the proposed naming convention, and adds some checks to test_runner.py that the number of tests violating the naming convention doesn't increase too much. Idea is this part of the change should not introduce merge conflicts or require much rebasing, so reviews of the complicated bits won't become invalidated too often; while the second part will just be file renames, which will require regular rebasing and will introduce merge conflicts with pending PRs, but can be merged later, and should also be much easier to review, since it will only include relatively trivial changes.
    
    Tree-SHA512: b96557d41714addbbfe2aed62fb5a48639eaeb1eb3aba30ac1b3a86bb3cb8d796c6247f9c414c4695c4bf54c0ec9968ac88e2f88fb62483bc1a2f89368f7fc80
    
    * update violation count
    
    Signed-off-by: pasta <pasta@dashboost.org>
    
    * Merge #11774: [tests] Rename functional tests
    
    6f881cc880 [tests] Remove EXPECTED_VIOLATION_COUNT (Anthony Towns)
    3150b3fea7 [tests] Rename misc functional tests. (Anthony Towns)
    81b79f2c39 [tests] Rename rpc_* functional tests. (Anthony Towns)
    61b8f7f273 [tests] Rename p2p_* functional tests. (Anthony Towns)
    90600bc7db [tests] Rename wallet_* functional tests. (Anthony Towns)
    ca6523d0c8 [tests] Rename feature_* functional tests. (Anthony Towns)
    
    Pull request description:
    
      This PR changes the functional tests to have a consistent naming scheme:
    
          tests for individual RPC methods are named rpc_...
          tests for interfaces (REST, ZMQ, RPC features) are named interface_...
          tests that explicitly test the p2p interface are named p2p_...
          tests for wallet features are named wallet_...
          tests for mining features are named mining_...
          tests for mempool behaviour are named mempool_...
          tests for full features that aren't wallet/mining/mempool are named feature_...
    
      Rationale: it's sometimes difficult for new contributors to know what's already covered by existing tests and where new tests should be added. Naming in a consistent fashion makes it easier to see what's already covered at a glance.
    
    Tree-SHA512: 4246790552d42bbd95f6d5bdf67702b81b3b2c583ce7eaf1fe6d8e254721279b47315973c6e9ae82dad6e4c747f12188160764bf2624c0f8f3b4d39330ec8b16
    
    * rename tests and edit associated strings to align test-suite with test name standards
    
    Signed-off-by: pasta <pasta@dashboost.org>
    
    * fix grammar in test/functional/test_runner.py
    
    Co-authored-by: dustinface <35775977+xdustinface@users.noreply.github.com>
    
    * ci: Fix excluded test names
    
    * rename feature_privatesend.py to rpc_privatesend.py
    
    Signed-off-by: pasta <pasta@dashboost.org>
    
    Co-authored-by: Wladimir J. van der Laan <laanwj@gmail.com>
    Co-authored-by: MarcoFalke <falke.marco@gmail.com>
    Co-authored-by: dustinface <35775977+xdustinface@users.noreply.github.com>
    Co-authored-by: xdustinface <xdustinfacex@gmail.com>

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -149,7 +149,7 @@ BASE_SCRIPTS= [
     'feature_minchainwork.py',
     'p2p_unrequested_blocks.py', # NOTE: needs axe_hash to pass
     'feature_shutdown.py',
-    'feature_privatesend.py',
+    'rpc_privatesend.py',
     'p2p_fingerprint.py',
     'feature_uacomment.py',
     'p2p_unrequested_blocks.py',
@@ -164,7 +164,7 @@ BASE_SCRIPTS= [
 EXTENDED_SCRIPTS = [
     # These tests are not run by the travis build process.
     # Longest test should go first, to favor running tests in parallel
-    'feature_pruning.py', # NOTE: Prune mode is incompatible with -txindex, should work governance validation disabled though.
+    'feature_pruning.py', # NOTE: Prune mode is incompatible with -txindex, should work with governance validation disabled though.
     # vv Tests less than 20m vv
     'feature_fee_estimation.py',
     # vv Tests less than 5m vv
