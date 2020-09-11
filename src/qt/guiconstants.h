c1e0abe44e8baee2daee410deaccb24526411f22
dustinface <35775977+xdustinface@users.noreply.github.com>
2020-09-11 14:07:12 +0200

    qt: Improved status bar (#3688)
    * qt: Adjust green color
    
    * qt: Don't disable the HD icon
    
    Disabling it leads to incorrect colors due to opacity.
    
    * qt: Move the HD icon to the left
    
    Reason: Its the only one not turning green by default if all is set
    properly. So this lets is look better imo.
    
    * qt: Increase statusbar icon size
    
    * qt: Show lock icon in status bar also if wallet is not encrypted
    
    Just as signal to the user!
    
    * qt: Refine connection icon usage
    
    * qt: Use state related colors in statusbar
    
    * qt: Hide the HD icon for non-HD wallets
    
    * qt: Update HD icon

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -13,7 +13,7 @@ static const int MODEL_UPDATE_DELAY = 250;
 static const int MAX_PASSPHRASE_SIZE = 1024;
 
 /* AxeGUI -- Size of icons in status bar */
-static const int STATUSBAR_ICONSIZE = 16;
+static const int STATUSBAR_ICONSIZE = 18;
 
 /* AxeGUI -- Size of button icons e.g. in SendCoinEntry or SignVerifyMessageDialog */
 static const int BUTTON_ICONSIZE = 23;
