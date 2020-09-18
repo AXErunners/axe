bytzck/BYTZ

qt: Introduce appearance tab and setup dialog (#3568)
* qt: Add "Appearance Tab" to OptionsDialog and move "Theme" into it

* qt: Add "Font scale" settings to Appearance Tab of OptionsDialog

Allows to scale the font in the same way like with -font-scale.

* qt: Add font weight settings to Appearance tab of OptionsDialog

Allows to set the weight for normal and bold text

* qt: Add font family setting to Appearance tab of OptionsDialog

Allows to choose between system default font or montserrat

* qt: GUIUtil - Maintain a map with supported weights for all fonts.

* qt: Introduce AppearanceWidget

A widget which just wraps all appearance related settings.

Also replaces Appearance settings in OptionsDialog with the introduced widget.

* qt: Introduce initial appearance setup dialog

This will pop up the first time the user starts the DashCore version with the new UI changes.

* qt: Load font related settings in GUIUtil::loadFonts

* qt: Make osDefaultFont global in GUIUtil to fix getFont()

Before getFont(FontFamily, ...) wasn't always able to return the correct font without the requested font family beeing set as application font upfront.

* qt: Improve supported weight helpers

Add conversion helpers. and let all of them depend on the currently selected font.

* qt: Default weights based on supported if not all weights are supported

For SystemDefault only because Montserrat has all supported weights.

* qt/test: Fix GUI tests

* Store normal/bold font GUI settings as "pure" (not "supported") values

* Rename supportedWeighti(To/From)Arg to supportedWeight(To/From)Index to better match the logic and avoid confusion with weight(To/From)Arg functions

Co-authored-by: UdjinM6 <UdjinM6@users.noreply.github.com>

src/qt/appearancewidget.cpp
@@ -1,0 +1,159 @@
+// Copyright (c) 2020 The Dash Core developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#if defined(HAVE_CONFIG_H)
+#include <config/dash-config.h>
+#endif
+
+#include <qt/forms/ui_appearancewidget.h>
+
+#include <qt/appearancewidget.h>
+#include <qt/optionsmodel.h>
+
+#include <util.h>
+
+#include <QComboBox>
+#include <QDataWidgetMapper>
+#include <QSettings>
+#include <QSlider>
+
+AppearanceWidget::AppearanceWidget(QWidget* parent) :
+    QWidget(parent),
+    ui(new Ui::AppearanceWidget),
+    fAcceptChanges(false),
+    prevTheme(GUIUtil::getActiveTheme()),
+    prevFontFamily(GUIUtil::getFontFamily()),
+    prevScale(GUIUtil::getFontScale()),
+    prevWeightNormal(GUIUtil::getFontWeightNormal()),
+    prevWeightBold(GUIUtil::getFontWeightBold())
+{
+    ui->setupUi(this);
+
+    for (const QString& entry : GUIUtil::listThemes()) {
+        ui->theme->addItem(entry, QVariant(entry));
+    }
+
+    GUIUtil::FontFamily fontSystem = GUIUtil::FontFamily::SystemDefault;
+    GUIUtil::FontFamily fontMontserrat = GUIUtil::FontFamily::Montserrat;
+
+    ui->fontFamily->addItem(GUIUtil::fontFamilyToString(fontSystem), QVariant(static_cast<int>(fontSystem)));
+    ui->fontFamily->addItem(GUIUtil::fontFamilyToString(fontMontserrat), QVariant(static_cast<int>(fontMontserrat)));
+
+    updateWeightSlider();
+
+    mapper = new QDataWidgetMapper(this);
+    mapper->setSubmitPolicy(QDataWidgetMapper::ManualSubmit);
+    mapper->setOrientation(Qt::Vertical);
+
+    connect(ui->theme, SIGNAL(currentTextChanged(const QString&)), this, SLOT(updateTheme(const QString&)));
+    connect(ui->fontFamily, SIGNAL(activated(int)), this, SLOT(updateFontFamily(int)));
+    connect(ui->fontScaleSlider, SIGNAL(valueChanged(int)), this, SLOT(updateFontScale(int)));
+    connect(ui->fontWeightNormalSlider, SIGNAL(valueChanged(int)), this, SLOT(updateFontWeightNormal(int)));
+    connect(ui->fontWeightBoldSlider, SIGNAL(valueChanged(int)), this, SLOT(updateFontWeightBold(int)));
+}
+
+AppearanceWidget::~AppearanceWidget()
+{
+    if (fAcceptChanges) {
+        mapper->submit();
+    } else {
+        if (prevTheme != GUIUtil::getActiveTheme()) {
+            updateTheme(prevTheme);
+        }
+        if (prevFontFamily != GUIUtil::getFontFamily()) {
+            GUIUtil::setFontFamily(prevFontFamily);
+        }
+        if (prevScale != GUIUtil::getFontScale()) {
+            GUIUtil::setFontScale(prevScale);
+        }
+        if (prevWeightNormal != GUIUtil::getFontWeightNormal()) {
+            GUIUtil::setFontWeightNormal(prevWeightNormal);
+        }
+        if (prevWeightBold != GUIUtil::getFontWeightBold()) {
+            GUIUtil::setFontWeightBold(prevWeightBold);
+        }
+    }
+    delete ui;
+}
+
+void AppearanceWidget::setModel(OptionsModel* _model)
+{
+    this->model = _model;
+
+    if (_model) {
+        mapper->setModel(_model);
+        mapper->addMapping(ui->theme, OptionsModel::Theme);
+        mapper->addMapping(ui->fontFamily, OptionsModel::FontFamily);
+        mapper->addMapping(ui->fontScaleSlider, OptionsModel::FontScale);
+        mapper->addMapping(ui->fontWeightNormalSlider, OptionsModel::FontWeightNormal);
+        mapper->addMapping(ui->fontWeightBoldSlider, OptionsModel::FontWeightBold);
+        mapper->toFirst();
+    }
+}
+
+void AppearanceWidget::accept()
+{
+    fAcceptChanges = true;
+}
+
+void AppearanceWidget::updateTheme(const QString& theme)
+{
+    QString newValue = theme.isEmpty() ? ui->theme->currentData().toString() : theme;
+    if (GUIUtil::getActiveTheme() != newValue) {
+        QSettings().setValue("theme", newValue);
+        GUIUtil::loadTheme();
+    }
+}
+
+void AppearanceWidget::updateFontFamily(int index)
+{
+    GUIUtil::setFontFamily(static_cast<GUIUtil::FontFamily>(ui->fontFamily->itemData(index).toInt()));
+    updateWeightSlider();
+}
+
+void AppearanceWidget::updateFontScale(int nScale)
+{
+    GUIUtil::setFontScale(nScale);
+}
+
+void AppearanceWidget::updateFontWeightNormal(int nValue, bool fForce)
+{
+    int nSliderValue = nValue;
+    if (nValue > ui->fontWeightBoldSlider->value() && !fForce) {
+        nSliderValue = ui->fontWeightBoldSlider->value();
+    }
+    const QSignalBlocker blocker(ui->fontWeightNormalSlider);
+    ui->fontWeightNormalSlider->setValue(nSliderValue);
+    GUIUtil::setFontWeightNormal(GUIUtil::supportedWeightFromIndex(ui->fontWeightNormalSlider->value()));
+}
+
+void AppearanceWidget::updateFontWeightBold(int nValue, bool fForce)
+{
+    int nSliderValue = nValue;
+    if (nValue < ui->fontWeightNormalSlider->value() && !fForce) {
+        nSliderValue = ui->fontWeightNormalSlider->value();
+    }
+    const QSignalBlocker blocker(ui->fontWeightBoldSlider);
+    ui->fontWeightBoldSlider->setValue(nSliderValue);
+    GUIUtil::setFontWeightBold(GUIUtil::supportedWeightFromIndex(ui->fontWeightBoldSlider->value()));
+}
+
+void AppearanceWidget::updateWeightSlider()
+{
+    int nMaximum = GUIUtil::getSupportedWeights().size() - 1;
+
+    ui->fontWeightNormalSlider->setMinimum(0);
+    ui->fontWeightNormalSlider->setMaximum(nMaximum);
+
+    ui->fontWeightBoldSlider->setMinimum(0);
+    ui->fontWeightBoldSlider->setMaximum(nMaximum);
+
+    if (nMaximum < 4) {
+        updateFontWeightNormal(0, true);
+        updateFontWeightBold(nMaximum, true);
+    } else {
+        updateFontWeightNormal(1, true);
+        updateFontWeightBold(4, true);
+    }
+}

src/qt/appearancewidget.h
@@ -1,0 +1,56 @@
+// Copyright (c) 2020 The Dash Core developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef DASH_QT_APPEARANCE_WIDGET_H
+#define DASH_QT_APPEARANCE_WIDGET_H
+
+#include <QWidget>
+
+#include <qt/guiutil.h>
+
+namespace Ui {
+class AppearanceWidget;
+}
+
+class OptionsModel;
+
+class QDataWidgetMapper;
+class QSlider;
+class QComboBox;
+
+class AppearanceWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit AppearanceWidget(QWidget* parent = 0);
+    ~AppearanceWidget();
+
+    void setModel(OptionsModel* model);
+
+public Q_SLOTS:
+    void accept();
+
+private Q_SLOTS:
+    void updateTheme(const QString& toTheme = QString());
+    void updateFontFamily(int index);
+    void updateFontScale(int nScale);
+    void updateFontWeightNormal(int nValue, bool fForce = false);
+    void updateFontWeightBold(int nValue, bool fForce = false);
+
+private:
+    Ui::AppearanceWidget* ui;
+    QDataWidgetMapper* mapper;
+    OptionsModel* model;
+    bool fAcceptChanges;
+    QString prevTheme;
+    int prevScale;
+    GUIUtil::FontFamily prevFontFamily;
+    QFont::Weight prevWeightNormal;
+    QFont::Weight prevWeightBold;
+
+    void updateWeightSlider();
+};
+
+#endif // DASH_QT_APPEARANCE_WIDGET_H

AXErunners/axe

src/qt/appearancewidget.cpp
@@ -1,0 +1,159 @@
+// Copyright (c) 2020 The Dash Core developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#if defined(HAVE_CONFIG_H)
+#include <config/axe-config.h>
+#endif
+
+#include <qt/forms/ui_appearancewidget.h>
+
+#include <qt/appearancewidget.h>
+#include <qt/optionsmodel.h>
+
+#include <util.h>
+
+#include <QComboBox>
+#include <QDataWidgetMapper>
+#include <QSettings>
+#include <QSlider>
+
+AppearanceWidget::AppearanceWidget(QWidget* parent) :
+    QWidget(parent),
+    ui(new Ui::AppearanceWidget),
+    fAcceptChanges(false),
+    prevTheme(GUIUtil::getActiveTheme()),
+    prevFontFamily(GUIUtil::getFontFamily()),
+    prevScale(GUIUtil::getFontScale()),
+    prevWeightNormal(GUIUtil::getFontWeightNormal()),
+    prevWeightBold(GUIUtil::getFontWeightBold())
+{
+    ui->setupUi(this);
+
+    for (const QString& entry : GUIUtil::listThemes()) {
+        ui->theme->addItem(entry, QVariant(entry));
+    }
+
+    GUIUtil::FontFamily fontSystem = GUIUtil::FontFamily::SystemDefault;
+    GUIUtil::FontFamily fontMontserrat = GUIUtil::FontFamily::Montserrat;
+
+    ui->fontFamily->addItem(GUIUtil::fontFamilyToString(fontSystem), QVariant(static_cast<int>(fontSystem)));
+    ui->fontFamily->addItem(GUIUtil::fontFamilyToString(fontMontserrat), QVariant(static_cast<int>(fontMontserrat)));
+
+    updateWeightSlider();
+
+    mapper = new QDataWidgetMapper(this);
+    mapper->setSubmitPolicy(QDataWidgetMapper::ManualSubmit);
+    mapper->setOrientation(Qt::Vertical);
+
+    connect(ui->theme, SIGNAL(currentTextChanged(const QString&)), this, SLOT(updateTheme(const QString&)));
+    connect(ui->fontFamily, SIGNAL(activated(int)), this, SLOT(updateFontFamily(int)));
+    connect(ui->fontScaleSlider, SIGNAL(valueChanged(int)), this, SLOT(updateFontScale(int)));
+    connect(ui->fontWeightNormalSlider, SIGNAL(valueChanged(int)), this, SLOT(updateFontWeightNormal(int)));
+    connect(ui->fontWeightBoldSlider, SIGNAL(valueChanged(int)), this, SLOT(updateFontWeightBold(int)));
+}
+
+AppearanceWidget::~AppearanceWidget()
+{
+    if (fAcceptChanges) {
+        mapper->submit();
+    } else {
+        if (prevTheme != GUIUtil::getActiveTheme()) {
+            updateTheme(prevTheme);
+        }
+        if (prevFontFamily != GUIUtil::getFontFamily()) {
+            GUIUtil::setFontFamily(prevFontFamily);
+        }
+        if (prevScale != GUIUtil::getFontScale()) {
+            GUIUtil::setFontScale(prevScale);
+        }
+        if (prevWeightNormal != GUIUtil::getFontWeightNormal()) {
+            GUIUtil::setFontWeightNormal(prevWeightNormal);
+        }
+        if (prevWeightBold != GUIUtil::getFontWeightBold()) {
+            GUIUtil::setFontWeightBold(prevWeightBold);
+        }
+    }
+    delete ui;
+}
+
+void AppearanceWidget::setModel(OptionsModel* _model)
+{
+    this->model = _model;
+
+    if (_model) {
+        mapper->setModel(_model);
+        mapper->addMapping(ui->theme, OptionsModel::Theme);
+        mapper->addMapping(ui->fontFamily, OptionsModel::FontFamily);
+        mapper->addMapping(ui->fontScaleSlider, OptionsModel::FontScale);
+        mapper->addMapping(ui->fontWeightNormalSlider, OptionsModel::FontWeightNormal);
+        mapper->addMapping(ui->fontWeightBoldSlider, OptionsModel::FontWeightBold);
+        mapper->toFirst();
+    }
+}
+
+void AppearanceWidget::accept()
+{
+    fAcceptChanges = true;
+}
+
+void AppearanceWidget::updateTheme(const QString& theme)
+{
+    QString newValue = theme.isEmpty() ? ui->theme->currentData().toString() : theme;
+    if (GUIUtil::getActiveTheme() != newValue) {
+        QSettings().setValue("theme", newValue);
+        GUIUtil::loadTheme();
+    }
+}
+
+void AppearanceWidget::updateFontFamily(int index)
+{
+    GUIUtil::setFontFamily(static_cast<GUIUtil::FontFamily>(ui->fontFamily->itemData(index).toInt()));
+    updateWeightSlider();
+}
+
+void AppearanceWidget::updateFontScale(int nScale)
+{
+    GUIUtil::setFontScale(nScale);
+}
+
+void AppearanceWidget::updateFontWeightNormal(int nValue, bool fForce)
+{
+    int nSliderValue = nValue;
+    if (nValue > ui->fontWeightBoldSlider->value() && !fForce) {
+        nSliderValue = ui->fontWeightBoldSlider->value();
+    }
+    const QSignalBlocker blocker(ui->fontWeightNormalSlider);
+    ui->fontWeightNormalSlider->setValue(nSliderValue);
+    GUIUtil::setFontWeightNormal(GUIUtil::supportedWeightFromIndex(ui->fontWeightNormalSlider->value()));
+}
+
+void AppearanceWidget::updateFontWeightBold(int nValue, bool fForce)
+{
+    int nSliderValue = nValue;
+    if (nValue < ui->fontWeightNormalSlider->value() && !fForce) {
+        nSliderValue = ui->fontWeightNormalSlider->value();
+    }
+    const QSignalBlocker blocker(ui->fontWeightBoldSlider);
+    ui->fontWeightBoldSlider->setValue(nSliderValue);
+    GUIUtil::setFontWeightBold(GUIUtil::supportedWeightFromIndex(ui->fontWeightBoldSlider->value()));
+}
+
+void AppearanceWidget::updateWeightSlider()
+{
+    int nMaximum = GUIUtil::getSupportedWeights().size() - 1;
+
+    ui->fontWeightNormalSlider->setMinimum(0);
+    ui->fontWeightNormalSlider->setMaximum(nMaximum);
+
+    ui->fontWeightBoldSlider->setMinimum(0);
+    ui->fontWeightBoldSlider->setMaximum(nMaximum);
+
+    if (nMaximum < 4) {
+        updateFontWeightNormal(0, true);
+        updateFontWeightBold(nMaximum, true);
+    } else {
+        updateFontWeightNormal(1, true);
+        updateFontWeightBold(4, true);
+    }
+}

src/qt/appearancewidget.h
@@ -1,0 +1,56 @@
+// Copyright (c) 2020 The Dash Core developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef AXE_QT_APPEARANCE_WIDGET_H
+#define AXE_QT_APPEARANCE_WIDGET_H
+
+#include <QWidget>
+
+#include <qt/guiutil.h>
+
+namespace Ui {
+class AppearanceWidget;
+}
+
+class OptionsModel;
+
+class QDataWidgetMapper;
+class QSlider;
+class QComboBox;
+
+class AppearanceWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit AppearanceWidget(QWidget* parent = 0);
+    ~AppearanceWidget();
+
+    void setModel(OptionsModel* model);
+
+public Q_SLOTS:
+    void accept();
+
+private Q_SLOTS:
+    void updateTheme(const QString& toTheme = QString());
+    void updateFontFamily(int index);
+    void updateFontScale(int nScale);
+    void updateFontWeightNormal(int nValue, bool fForce = false);
+    void updateFontWeightBold(int nValue, bool fForce = false);
+
+private:
+    Ui::AppearanceWidget* ui;
+    QDataWidgetMapper* mapper;
+    OptionsModel* model;
+    bool fAcceptChanges;
+    QString prevTheme;
+    int prevScale;
+    GUIUtil::FontFamily prevFontFamily;
+    QFont::Weight prevWeightNormal;
+    QFont::Weight prevWeightBold;
+
+    void updateWeightSlider();
+};
+
+#endif // AXE_QT_APPEARANCE_WIDGET_H