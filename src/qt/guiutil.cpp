bytzck/BYTZ

qt: Introduce runtime theme changes (#3559)
* qt: Set the default theme properly

* qt: Keep track of disabled rects for macOS

This allows enabling them again on theme changes

* qt: Introduce runtime theme changes

Runtime theme changes means no more client restart required if the
theme gets changed in the options dialog.

In the RPCConsole's StyleChange event make sure following things are still correct after a runtime theme change:
- Hide prompt icon for dash themes in rpc console if dash theme gets
activated.
- Clear rpc console on theme changes to make sure fonts/sizes/colors are correct.

src/qt/guiutil.cpp
@@ -132,6 +132,11 @@ static std::set<QWidget*> setFixedPitchFontUpdates;
 // Contains all widgets where a non-default fontsize has been seet with GUIUtil::setFont
 static std::map<QWidget*, int> mapFontSizeUpdates;
 
+#ifdef Q_OS_MAC
+// Contains all widgets where the macOS focus rect has been disabled.
+static std::set<QWidget*> setRectsDisabled;
+#endif
+
 static const std::map<ThemedColor, QColor> themedColors = {
     { ThemedColor::DEFAULT, QColor(85, 85, 85) },
     { ThemedColor::UNCONFIRMED, QColor(128, 128, 128) },
@@ -1025,7 +1030,12 @@ const std::vector<QString> listThemes()
     return vecThemes;
 }
 
-void loadStyleSheet(QWidget* widget, bool fDebugWidget)
+const QString getDefaultTheme()
+{
+    return defaultTheme;
+}
+
+void loadStyleSheet(QWidget* widget, bool fForceUpdate)
 {
     AssertLockNotHeld(cs_css);
     LOCK(cs_css);
@@ -1036,7 +1046,7 @@ void loadStyleSheet(QWidget* widget, bool fDebugWidget)
     bool fDebugCustomStyleSheets = gArgs.GetBoolArg("-debug-ui", false) && isStyleSheetDirectoryCustom();
     bool fStyleSheetChanged = false;
 
-    if (stylesheet == nullptr || fDebugCustomStyleSheets) {
+    if (stylesheet == nullptr || fForceUpdate || fDebugCustomStyleSheets) {
         auto hasModified = [](const std::vector<QString>& vecFiles) -> bool {
             static std::map<const QString, QDateTime> mapLastModified;
 
@@ -1053,7 +1063,7 @@ void loadStyleSheet(QWidget* widget, bool fDebugWidget)
         };
 
         auto loadFiles = [&](const std::vector<QString>& vecFiles) -> bool {
-            if (fDebugCustomStyleSheets && !hasModified(vecFiles)) {
+            if (!fForceUpdate && fDebugCustomStyleSheets && !hasModified(vecFiles)) {
                 return false;
             }
 
@@ -1086,29 +1096,27 @@ void loadStyleSheet(QWidget* widget, bool fDebugWidget)
         fStyleSheetChanged = loadFiles(vecFiles);
     }
 
-    bool fUpdateStyleSheet = fDebugCustomStyleSheets && fStyleSheetChanged;
-
-    if (fDebugWidget) {
-        setWidgets.insert(widget);
-        QWidgetList allWidgets = QApplication::allWidgets();
-        auto it = setWidgets.begin();
-        while (it != setWidgets.end()) {
-            if (!allWidgets.contains(*it)) {
-                it = setWidgets.erase(it);
-                continue;
-            }
-            if (fUpdateStyleSheet && *it != widget) {
-                (*it)->setStyleSheet(*stylesheet);
-            }
-            ++it;
-        }
-    }
+    bool fUpdateStyleSheet = fForceUpdate || (fDebugCustomStyleSheets && fStyleSheetChanged);
 
     if (widget) {
+        setWidgets.insert(widget);
         widget->setStyleSheet(*stylesheet);
     }
 
-    if (!ShutdownRequested() && fDebugCustomStyleSheets) {
+    QWidgetList allWidgets = QApplication::allWidgets();
+    auto it = setWidgets.begin();
+    while (it != setWidgets.end()) {
+        if (!allWidgets.contains(*it)) {
+            it = setWidgets.erase(it);
+            continue;
+        }
+        if (fUpdateStyleSheet && *it != widget) {
+            (*it)->setStyleSheet(*stylesheet);
+        }
+        ++it;
+    }
+
+    if (!ShutdownRequested() && fDebugCustomStyleSheets && !fForceUpdate) {
         QTimer::singleShot(200, [] { loadStyleSheet(); });
     }
 }
@@ -1517,12 +1525,36 @@ bool dashThemeActive()
     return theme != traditionalTheme;
 }
 
+void loadTheme(QWidget* widget, bool fForce)
+{
+    loadStyleSheet(widget, fForce);
+    updateFonts();
+    updateMacFocusRects();
+}
+
 void disableMacFocusRect(const QWidget* w)
 {
 #ifdef Q_OS_MAC
     for (const auto& c : w->findChildren<QWidget*>()) {
         if (c->testAttribute(Qt::WA_MacShowFocusRect)) {
             c->setAttribute(Qt::WA_MacShowFocusRect, !dashThemeActive());
+            setRectsDisabled.emplace(c);
+        }
+    }
+#endif
+}
+
+void updateMacFocusRects()
+{
+#ifdef Q_OS_MAC
+    QWidgetList allWidgets = QApplication::allWidgets();
+    auto it = setRectsDisabled.begin();
+    while (it != setRectsDisabled.end()) {
+        if (allWidgets.contains(*it)) {
+            (*it)->setAttribute(Qt::WA_MacShowFocusRect, !dashThemeActive());
+            ++it;
+        } else {
+            it = setRectsDisabled.erase(it);
         }
     }
 #endif

src/qt/rpcconsole.cpp
@@ -1263,6 +1263,16 @@ void RPCConsole::hideEvent(QHideEvent *event)
     clientModel->getPeerTableModel()->stopAutoRefresh();
 }
 
+void RPCConsole::changeEvent(QEvent* e)
+{
+    if (e->type() == QEvent::StyleChange) {
+        clear();
+        ui->promptIcon->setHidden(GUIUtil::dashThemeActive());
+    }
+
+    QWidget::changeEvent(e);
+}
+
 void RPCConsole::showPeersTableContextMenu(const QPoint& point)
 {
     QModelIndex index = ui->peerWidget->indexAt(point);

AXErunners/axe

src/qt/guiutil.cpp
@@ -139,6 +139,11 @@ static std::set<QWidget*> setFixedPitchFontUpdates;
 // Contains all widgets where a non-default fontsize has been seet with GUIUtil::setFont
 static std::map<QWidget*, int> mapFontSizeUpdates;
 
+#ifdef Q_OS_MAC
+// Contains all widgets where the macOS focus rect has been disabled.
+static std::set<QWidget*> setRectsDisabled;
+#endif
+
 static const std::map<ThemedColor, QColor> themedColors = {
     { ThemedColor::DEFAULT, QColor(85, 85, 85) },
     { ThemedColor::UNCONFIRMED, QColor(128, 128, 128) },
@@ -1064,7 +1069,12 @@ const std::vector<QString> listThemes()
     return vecThemes;
 }
 
-void loadStyleSheet(QWidget* widget, bool fDebugWidget)
+const QString getDefaultTheme()
+{
+    return defaultTheme;
+}
+
+void loadStyleSheet(QWidget* widget, bool fForceUpdate)
 {
     AssertLockNotHeld(cs_css);
     LOCK(cs_css);
@@ -1075,7 +1085,7 @@ void loadStyleSheet(QWidget* widget, bool fDebugWidget)
     bool fDebugCustomStyleSheets = gArgs.GetBoolArg("-debug-ui", false) && isStyleSheetDirectoryCustom();
     bool fStyleSheetChanged = false;
 
-    if (stylesheet == nullptr || fDebugCustomStyleSheets) {
+    if (stylesheet == nullptr || fForceUpdate || fDebugCustomStyleSheets) {
         auto hasModified = [](const std::vector<QString>& vecFiles) -> bool {
             static std::map<const QString, QDateTime> mapLastModified;
 
@@ -1092,7 +1102,7 @@ void loadStyleSheet(QWidget* widget, bool fDebugWidget)
         };
 
         auto loadFiles = [&](const std::vector<QString>& vecFiles) -> bool {
-            if (fDebugCustomStyleSheets && !hasModified(vecFiles)) {
+            if (!fForceUpdate && fDebugCustomStyleSheets && !hasModified(vecFiles)) {
                 return false;
             }
 
@@ -1125,29 +1135,27 @@ void loadStyleSheet(QWidget* widget, bool fDebugWidget)
         fStyleSheetChanged = loadFiles(vecFiles);
     }
 
-    bool fUpdateStyleSheet = fDebugCustomStyleSheets && fStyleSheetChanged;
-
-    if (fDebugWidget) {
-        setWidgets.insert(widget);
-        QWidgetList allWidgets = QApplication::allWidgets();
-        auto it = setWidgets.begin();
-        while (it != setWidgets.end()) {
-            if (!allWidgets.contains(*it)) {
-                it = setWidgets.erase(it);
-                continue;
-            }
-            if (fUpdateStyleSheet && *it != widget) {
-                (*it)->setStyleSheet(*stylesheet);
-            }
-            ++it;
-        }
-    }
+    bool fUpdateStyleSheet = fForceUpdate || (fDebugCustomStyleSheets && fStyleSheetChanged);
 
     if (widget) {
+        setWidgets.insert(widget);
         widget->setStyleSheet(*stylesheet);
     }
 
-    if (!ShutdownRequested() && fDebugCustomStyleSheets) {
+    QWidgetList allWidgets = QApplication::allWidgets();
+    auto it = setWidgets.begin();
+    while (it != setWidgets.end()) {
+        if (!allWidgets.contains(*it)) {
+            it = setWidgets.erase(it);
+            continue;
+        }
+        if (fUpdateStyleSheet && *it != widget) {
+            (*it)->setStyleSheet(*stylesheet);
+        }
+        ++it;
+    }
+
+    if (!ShutdownRequested() && fDebugCustomStyleSheets && !fForceUpdate) {
         QTimer::singleShot(200, [] { loadStyleSheet(); });
     }
 }
@@ -1556,12 +1564,36 @@ bool axeThemeActive()
     return theme != traditionalTheme;
 }
 
+void loadTheme(QWidget* widget, bool fForce)
+{
+    loadStyleSheet(widget, fForce);
+    updateFonts();
+    updateMacFocusRects();
+}
+
 void disableMacFocusRect(const QWidget* w)
 {
 #ifdef Q_OS_MAC
     for (const auto& c : w->findChildren<QWidget*>()) {
         if (c->testAttribute(Qt::WA_MacShowFocusRect)) {
             c->setAttribute(Qt::WA_MacShowFocusRect, !axeThemeActive());
+            setRectsDisabled.emplace(c);
+        }
+    }
+#endif
+}
+
+void updateMacFocusRects()
+{
+#ifdef Q_OS_MAC
+    QWidgetList allWidgets = QApplication::allWidgets();
+    auto it = setRectsDisabled.begin();
+    while (it != setRectsDisabled.end()) {
+        if (allWidgets.contains(*it)) {
+            (*it)->setAttribute(Qt::WA_MacShowFocusRect, !axeThemeActive());
+            ++it;
+        } else {
+            it = setRectsDisabled.erase(it);
         }
     }
 #endif

src/qt/rpcconsole.cpp
@@ -1267,6 +1267,16 @@ void RPCConsole::hideEvent(QHideEvent *event)
     clientModel->getPeerTableModel()->stopAutoRefresh();
 }
 
+void RPCConsole::changeEvent(QEvent* e)
+{
+    if (e->type() == QEvent::StyleChange) {
+        clear();
+        ui->promptIcon->setHidden(GUIUtil::axeThemeActive());
+    }
+
+    QWidget::changeEvent(e);
+}
+
 void RPCConsole::showPeersTableContextMenu(const QPoint& point)
 {
     QModelIndex index = ui->peerWidget->indexAt(point);